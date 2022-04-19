1280a07cec218f26fc6b560c7cfb9ed234014f25
smallSohoSolo <b3069741@gmail.com>
2022-04-19 14:38:52 +0800

    feat: init project

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./main/App"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
