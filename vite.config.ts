e9821264ff1426634a2c20180e6812a841f03093
AruSeito <weichen@illasoft.com>
2022-04-20 16:53:32 +0800

    build: add alias for componentManager

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,4 +22,10 @@ export default defineConfig({
       include: ["**/*.tsx", "**/*.ts"],
     }),
   ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "@componentManager": resolve(__dirname, "./src/componentManager"),
+    },
+  },
 })
