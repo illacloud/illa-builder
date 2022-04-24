83ce1796158f2222e9e147ce4692cd2da85a4801
AruSeito <weichen@illasoft.com>
2022-04-24 14:47:12 +0800

    test: fix Label test

--- src/componentManager/components/InputRow/tests/label.e2e.tsx
+++ src/componentManager/components/InputRow/tests/label.e2e.tsx
@@ -2,7 +2,13 @@ import Label from "../label"
 import { mount, unmount } from "@cypress/react"
 import "@testing-library/cypress"
 
-it("Trigger renders without padding", () => {
+it("Render Label without description", () => {
+  mount(<Label labelName="testName" />)
+  cy.findByText("testName").should("exist")
+  unmount()
+})
+
+it("Render Label with description", () => {
   mount(<Label labelName="testName" labelDesc="testDesc" />)
   cy.findByText("testName").trigger("mouseover")
   cy.findByText("testDesc").should("exist")
--- src/componentManager/components/InputRow/tests/label.test.tsx
+++
@@ -1,8 +0,0 @@
-import Label from "../label"
-import { render, screen } from "@testing-library/react"
-import "@testing-library/jest-dom"
-
-test("Render Label without description", () => {
-  render(<Label labelName="testName" />)
-  expect(screen.getByText("testName")).toBeInTheDocument()
-})
