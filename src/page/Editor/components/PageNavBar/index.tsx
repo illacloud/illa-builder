5e492115feccea9703b419e647f9b4a6a8fc8463
xiaoyu <lavamint@outlook.com>
2022-04-24 15:41:26 +0800

    feat: add i18n

--- src/page/Editor/components/PageNavBar/index.tsx
+++ src/page/Editor/components/PageNavBar/index.tsx
@@ -1,18 +1,29 @@
 import { FC, HTMLAttributes } from "react"
 import { Button, ButtonGroup } from "@illa-design/button"
 import { MoreIcon } from "@illa-design/icon"
+import { useTranslation } from "react-i18next"
 import { NavBarStyle } from "./style"
+import { Select } from "@illa-design/select"
 
 interface PageNavBarProps extends HTMLAttributes<HTMLDivElement> {}
 
 export const PageNavBar: FC<PageNavBarProps> = (props) => {
   const { className } = props
+  const { t, i18n } = useTranslation()
 
   return (
     <div className={className} css={NavBarStyle}>
       <ButtonGroup spacing={"8px"}>
         <Button colorScheme={"gray"} leftIcon={<MoreIcon />}></Button>
-        <Button colorScheme={"purple"}>deploy</Button>
+        <Button colorScheme={"purple"}>{t("deploy")}</Button>
+        <Button
+          colorScheme={"purple"}
+          onClick={() => {
+            i18n.changeLanguage("zh")
+          }}
+        >
+          changeLanguage
+        </Button>
       </ButtonGroup>
     </div>
   )
