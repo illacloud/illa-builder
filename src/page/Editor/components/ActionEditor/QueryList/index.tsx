4d7a908871e9ce9d0f615e53bbf17b12cea672f4
Spike <l-yanlei@hotmail.com>
2022-05-05 15:20:41 +0800

    feat: add basic querylist structure

--- src/page/Editor/components/ActionEditor/QueryList/index.tsx
+++ src/page/Editor/components/ActionEditor/QueryList/index.tsx
@@ -1,29 +1,80 @@
-import { FC, HTMLAttributes } from "react"
+import { FC, HTMLAttributes, useState } from "react"
 import {
   QueryListContainer,
   QueryListHeader,
+  FilterIconCss,
+  HeaderTitle,
   NewButton,
+  NewButtonContentWrapper,
   AddIconInNewButton,
+  QueryItemList,
+  QueryItem,
+  QueryItemIcon,
+  QueryItemTitle,
+  WarningIndicator,
+  UpdatedIndicator,
+  QueryItemTime,
 } from "./style"
 import { Button } from "@illa-design/button"
-import { AddIcon, SearchIcon } from "@illa-design/icon"
+import {
+  AddIcon,
+  SearchIcon,
+  ImageDefaultIcon,
+  FilterIcon,
+  WarningCircleIcon,
+} from "@illa-design/icon"
 
 interface QueryListProps extends HTMLAttributes<HTMLDivElement> { }
 
 export const QueryList: FC<QueryListProps> = (props) => {
   const { className } = props
+  const [queryItems, setQueryItems] = useState(
+    Array.from({ length: 10 }, (_, index) => {
+      return {
+        type: "rest",
+        name: `Query_${index}`,
+        isUpdated: index % 2 === 0,
+        isWarning: index % 2 === 0,
+        time: "0.7s",
+      }
+    }),
+  )
+
+  const queryItemsList = queryItems.map((item, index) => {
+    const { name, time, isWarning, isUpdated } = item
+    const icon = <ImageDefaultIcon />
+
+    return (
+      <li key={`${name}_${index}`} css={QueryItem}>
+        <span css={QueryItemIcon}>
+          {icon}
+          {isWarning && (
+            <WarningCircleIcon css={WarningIndicator} size={"8px"} />
+          )}
+        </span>
+        <span css={QueryItemTitle}>
+          {name}
+          {isUpdated && <span css={UpdatedIndicator}></span>}
+        </span>
+        <span css={QueryItemTime}>{time}</span>
+      </li>
+    )
+  })
 
   return (
     <div className={className} css={QueryListContainer}>
       <header css={QueryListHeader}>
-        Queries List
-        <SearchIcon />
+        <span css={HeaderTitle}>Queries List</span>
+        <SearchIcon size={"12px"} />
+        <FilterIcon size={"12px"} css={FilterIconCss} />
       </header>
       <Button css={NewButton}>
-        <AddIcon css={AddIconInNewButton} />
-        New
+        <span css={NewButtonContentWrapper}>
+          <AddIcon css={AddIconInNewButton} />
+          New
+        </span>
       </Button>
-      <ul></ul>
+      <ul css={QueryItemList}>{queryItemsList}</ul>
     </div>
   )
 }
--- src/page/Editor/components/ActionEditor/QueryList/style.tsx
+++ src/page/Editor/components/ActionEditor/QueryList/style.tsx
@@ -1,4 +1,4 @@
-import { css } from "@emotion/react"
+import { css, SerializedStyles } from "@emotion/react"
 import { globalColor, illaPrefix } from "@illa-design/theme"
 
 export const QueryListContainer = css`
@@ -6,7 +6,7 @@ export const QueryListContainer = css`
   flex-direction: column;
   width: 255px;
   border-right: 1px solid ${globalColor(`--${illaPrefix}-grayBlue-08`)};
-  `
+`
 
 export const QueryListHeader = css`
   height: 48px;
@@ -15,17 +15,92 @@ export const QueryListHeader = css`
   padding: 13px 16px;
   color: ${globalColor(`--${illaPrefix}-grayBlue-06`)};
   justify-content: space-between;
-  `
+`
+
+export const HeaderTitle = css`
+  flex: 1;
+`
 
 export const NewButton = css`
-  background-color: ${globalColor(`--${illaPrefix}-purple-07`)};
-  color: ${globalColor(`--${illaPrefix}-purple-02`)};
+  background-color: ${globalColor(`--${illaPrefix}-brand-purple-07`)}!important;
+  color: ${globalColor(`--${illaPrefix}-brand-purple-01`)}!important;
   justify-content: center;
   font-size: 14px;
   margin: 0 16px;
   height: 32px;
-  `
+  border-radius: 8px !important;
+  line-height: 32px;
+  flex-shrink: 0;
+
+  & > * {
+    font-size: 14px !important;
+  }
+
+  &:hover {
+    background-color: ${globalColor(
+  `--${illaPrefix}-brand-purple-06`,
+)}!important;
+  }
+`
+
+export const FilterIconCss = css`
+  margin-left: 10px;
+`
+
+export const NewButtonContentWrapper = css`
+  display: flex;
+  align-items: center;
+`
 
 export const AddIconInNewButton = css`
   margin-right: 8px;
+`
+
+export const QueryItemList = css`
+  list-style: none;
+  line-height: 40px;
+  padding: 9px 16px;
+  margin: 0;
+  overflow: auto;
+`
+
+export const QueryItem = css`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`
+
+export const QueryItemIcon = css`
+  position: relative;
+`
+
+export const QueryItemTitle = css`
+  color: ${globalColor(`--${illaPrefix}-grayBlue-02`)};
+  flex: 1 1 0;
+  display: flex;
+  align-items: center;
+  margin-left: 8px;
+`
+
+export const WarningIndicator = css`
+  position: absolute;
+  color: ${globalColor(`--${illaPrefix}-orange-06`)};
+  bottom: 10px;
+  right: 0;
+
+  & > path:last-child {
+    fill: ${globalColor(`--${illaPrefix}-orange-03`)};
+  }
+`
+
+export const UpdatedIndicator = css`
+  display: inline-block;
+  height: 6px;
+  width: 6px;
+  border-radius: 50%;
+  background-color: ${globalColor(`--${illaPrefix}-blue-03`)};
+  margin-left: 10px;
+`
+export const QueryItemTime = css`
+  color: ${globalColor(`--${illaPrefix}-grayBlue-06`)};
 `
