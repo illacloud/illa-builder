a08aa724f6cc5ce8132ba3f031eaebca5ef9b699
xiaoyu <lavamint@outlook.com>
2022-05-05 15:14:52 +0800

    feat: update dnd

--- src/page/Editor/components/PageNavBar/style.tsx
+++ src/page/Editor/components/PageNavBar/style.tsx
@@ -7,5 +7,5 @@ export const NavBarStyle = css`
   flex-direction: row;
   justify-content: flex-end;
   border-bottom: 1px solid ${globalColor(`--${illaPrefix}-grayblue-08`)};
-  padding-right: 17px;
+  padding: 8px 17px 8px 0;
 `
