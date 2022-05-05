5f200272a4dd463fc0150a60afb3eaaf1ed4df6b
xiaoyu <lavamint@outlook.com>
2022-05-05 16:36:14 +0800

    feat: add middleware

--- src/store.ts
+++ src/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
+import logger from 'redux-logger'
 import { rootReducer } from "@/reducers"
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 })
 
 export default store
