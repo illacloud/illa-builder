4bca73f599487908e143f60a3f59d8a69094f217
xiaoyu <lavamint@outlook.com>
2022-05-09 19:24:00 +0800

    feat: add DragLayerComponent

---
+++ src/page/Editor/components/CanvasContainer/DragLayerComponent/index.tsx
@@ -0,0 +1,14 @@
+import { HTMLAttributes } from "react"
+import { applyDragLayer } from "./style"
+
+export interface DragLayerProps extends HTMLAttributes<HTMLDivElement> {
+  rowHeight: number
+  columnWidth: number
+  noPad: boolean
+}
+
+export function DragLayerComponent(props: DragLayerProps) {
+  return <div css={applyDragLayer(props)} />
+}
+
+DragLayerComponent.displayName = "DragLayerComponent"
