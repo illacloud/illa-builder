e63bc32c8f7500edb71f51f2b41e18c1eb78d5bd
AruSeito <weichen@illasoft.com>
2022-05-11 01:20:26 +0800

    feat: render config panel

--- src/page/Editor/components/PanelSetters/interface.ts
+++ src/page/Editor/components/PanelSetters/interface.ts
@@ -9,13 +9,13 @@ export interface PanelFieldConfig {
   attrName: string
   placeholder?: string
   options?: any[]
-  // events?:event[] // ????待定，事件相关的，还没敲定，先不理他
+  // events?:event[] // TODO:
 }
 
 export interface PanelFieldGroupConfig {
-  id: string // 随机生成
-  isOpened?: boolean // 默认为 true
-  groupName: "Basic" | "Styled" | "Event" // 看情况拓展成对象，来搞多语言，但凡展示的都这样考虑
+  id: string
+  isOpened?: boolean
+  groupName: "Basic" | "Styled" | "Event"
   children: PanelFieldConfig[] | PanelFieldGroupConfig[]
 }
 
