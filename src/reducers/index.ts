dc20c78397714a159b12288603f0911b1d904e74
xiaoyu <lavamint@outlook.com>
2022-05-11 17:46:47 +0800

    feat: add reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -3,9 +3,11 @@ import { combineReducers } from "redux"
 import countReducer from "./DataWorkspace/count"
 import dslReducer from "./CanvasContainer/dslReducer"
 import editorReducer from "./CanvasContainer/editorReducer"
+import widgetStatesReducer  from "./CanvasContainer/widgetStatesReducer"
 
 export const rootReducer = combineReducers({
   counter: countReducer,
   dslState: dslReducer,
   editor: editorReducer,
+  widgetStates: widgetStatesReducer,
 })
