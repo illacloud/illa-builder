128a90c394a67cb1964639164cc882a6fa19dbc3
AruSeito <weichen@illasoft.com>
2022-05-20 18:34:25 +0800

    fix: select test error

--- src/wrappedComponents/Select/index.tsx
+++ src/wrappedComponents/Select/index.tsx
@@ -2,16 +2,33 @@ import { FC } from "react"
 import { SelectProps, WrappedSelectProps } from "./interface"
 import { Wrapper } from "../Wrapper"
 import { Select } from "@illa-design/select"
-import LabelProps from "../Label/interface"
 import Label from "../Label"
 
 export const WrappedSelect: FC<WrappedSelectProps> = (props) => {
   const { optionConfigureMode, showClear } = props
-  const labelProps: LabelProps = props
+  const {
+    label,
+    labelAlign,
+    labelCaption,
+    labelPosition,
+    labelWidth,
+    labelWidthUnit,
+    required,
+    hidden,
+  } = props
   const selectProps: SelectProps = props
   return (
     <Wrapper>
-      <Label {...labelProps} />
+      <Label
+        label={label}
+        labelAlign={labelAlign}
+        labelCaption={labelCaption}
+        labelPosition={labelPosition}
+        labelWidth={labelWidth}
+        labelWidthUnit={labelWidthUnit}
+        required={required}
+        hidden={hidden}
+      />
       <Select allowClear={showClear} {...selectProps} />
     </Wrapper>
   )
