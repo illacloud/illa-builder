671ff2a27398a07b53778078665b49763cf8fe39
zch <zhengchunhui@illasoft.com>
2022-05-20 15:27:02 +0800

    feat: cm css

--- src/components/EditorInput/interface.ts
+++ src/components/EditorInput/interface.ts
@@ -1,6 +1,9 @@
+import { SerializedStyles } from "@emotion/react"
+
 type EditorMode = "javascript" | "sql"
 
 export interface EditorInputProps {
+  _css?: SerializedStyles
   mode: EditorMode
   lineNumbers?: boolean
   height?: string
