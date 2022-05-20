5e5ab40db6b57af87e6c02fe84d9eeb8e920d8e4
AruSeito <weichen@illasoft.com>
2022-05-20 17:48:19 +0800

    fix: comment

--- src/page/Editor/components/PanelSetters/index.tsx
+++ src/page/Editor/components/PanelSetters/index.tsx
@@ -1,9 +1,8 @@
-import ListSetter from "./ListSetter"
-import BaseInput from "./InputSetter/baseInput"
-import BaseRadioGroupSetter from "./RadioGroupSetter/baseRadioGroup"
-// import EventHandlerSetter from "./EventHandlerSetter"
-import BaseSwitchSetter from "./SwitchSetter/baseSwitch"
-import DynamicSwitchSetter from "./SwitchSetter/dynamicSwitch"
+import { ListSetter } from "./ListSetter"
+import { BaseInput } from "./InputSetter/baseInput"
+import { BaseRadioGroupSetter } from "./RadioGroupSetter/baseRadioGroup"
+import { BaseSwitchSetter } from "./SwitchSetter/baseSwitch"
+import { DynamicSwitchSetter } from "./SwitchSetter/dynamicSwitch"
 
 const SetterTypeMapSetter = {
   INPUT_SETTER: BaseInput,
@@ -11,7 +10,6 @@ const SetterTypeMapSetter = {
   SWITCH_SETTER: BaseSwitchSetter,
   LIST_SETTER: ListSetter,
   DYNAMIC_SWITCH_SETTER: DynamicSwitchSetter,
-  // EVENT_HANDLER_SETTER: EventHandlerSetter,
 }
 
 export type SetterType = keyof typeof SetterTypeMapSetter
