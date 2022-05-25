2573fdc517ee661f632b620d30f16eed815d02e7
xiaoyu <lavamint@outlook.com>
2022-05-25 18:42:28 +0800

    feat: update state

--- src/redux/editor/widgetStates/widgetStateSelector.ts
+++ src/redux/editor/widgetStates/widgetStateSelector.ts
@@ -4,3 +4,6 @@ export const getWidgetStates = (state: RootState) => state.editor.widgetStates
 
 export const getFocusedWidget = (state: RootState) =>
   state.editor.widgetStates.focusedWidget
+
+export const getDragDetails = (state: RootState) =>
+    state.editor.widgetStates.dragDetails
