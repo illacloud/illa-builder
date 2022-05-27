52293133aa6fff54ae3d3985743e6fd24df9344e
xiaoyu <lavamint@outlook.com>
2022-05-27 14:28:19 +0800

    style: delete

--- src/page/Editor/components/PageNavBar/ZoomControl/style.tsx
+++ src/page/Editor/components/PageNavBar/ZoomControl/style.tsx
@@ -1,6 +1,5 @@
 import { css } from "@emotion/react"
 import { globalColor, illaPrefix } from "@illa-design/theme"
-import { ZoomControl } from "@/page/Editor/components/PageNavBar/ZoomControl/index"
 
 export const controlStyle = css`
   display: flex;
