150023bb275c6b1c9d169e27203ee43fc8894d05
xiaoyu <lavamint@outlook.com>
2022-06-01 16:08:32 +0800

    feat: add resources title

---
+++ src/page/Dashboard/DashboardResources/style.tsx
@@ -0,0 +1,11 @@
+import { SerializedStyles } from "@emotion/serialize"
+import { css } from "@emotion/react"
+
+export const resourceContainerStyle: SerializedStyles = css`
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 67%;
+  height: 100%;
+`
