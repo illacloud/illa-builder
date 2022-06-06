05ac63f69673c2f0921f292ff5bf73ad15ac2499
Jarvey <haojielin.linger@gmail.com>
2022-06-06 11:22:01 +0800

    fix: update transformer style file location

--- src/page/App/components/ActionEditor/ActionEditorPanel/ResourceEditor/Transformer/index.tsx
+++ src/page/App/components/ActionEditor/ActionEditorPanel/ResourceEditor/Transformer/index.tsx
@@ -11,7 +11,7 @@ import {
   radioBtnStyle,
 } from "@/page/App/components/ActionEditor/ActionEditorPanel/style"
 import { EditorInput } from "@/components/EditorInput"
-import { disableTransformerStyle } from "@/page/App/components/ActionEditor/ActionEditorPanel/Transformer/style"
+import { disableTransformerStyle } from "./style"
 
 export const Transformer = () => {
   const { t } = useTranslation()
