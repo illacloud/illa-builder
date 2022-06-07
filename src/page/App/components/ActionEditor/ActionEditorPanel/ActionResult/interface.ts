1134f30c321d544b92c837101852306e1e893da4
Spike <l-yanlei@hotmail.com>
2022-06-07 11:51:15 +0800

    feat: add animation for ActionResult

--- src/page/App/components/ActionEditor/ActionEditorPanel/ActionResult/interface.ts
+++ src/page/App/components/ActionEditor/ActionEditorPanel/ActionResult/interface.ts
@@ -1,6 +1,8 @@
+import { HTMLAttributes } from "react"
+
 export type ActionRestultStatus = "success" | "error"
 
-export interface ActionResultProps {
+export interface ActionResultProps extends HTMLAttributes<HTMLDivElement> {
   result?: string
   type?: string
   status?: ActionRestultStatus
