88f4fbe951dddea58834d92981a4f9a635e3ebcc
smallSohoSolo <b3069741@gmail.com>
2022-06-14 12:59:55 +0800

    fix: remove zoom

--- src/page/App/components/CanvasPanel/style.ts
+++ src/page/App/components/CanvasPanel/style.ts
@@ -5,9 +5,9 @@ export function applyScaleContainerStyle(scale: number): SerializedStyles {
   return css`
     transform: scale(${scale / 100});
     transform-origin: 50% 0;
+    overflow: auto;
     background: ${globalColor(`--${illaPrefix}-white-01`)};
     box-sizing: border-box;
-    overflow: hidden;
     height: 100%;
     width: 100%;
   `
