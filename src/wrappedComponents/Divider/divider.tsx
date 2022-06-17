6e8b333d0336b9ab5e01e6b6c0eff9ce8da77469
aoao <55166103+Z-AOAO@users.noreply.github.com>
2022-06-17 14:14:24 +0800

    feat: add wrapped divider

---
+++ src/wrappedComponents/Divider/divider.tsx
@@ -0,0 +1,35 @@
+import { FC, useMemo } from "react"
+import { Wrapper } from "@/wrappedComponents/Wrapper"
+import { withParser } from "@/wrappedComponents/parserHOC"
+import { TooltipWrapper } from "@/wrappedComponents/TooltipWrapper"
+import { Divider } from "@illa-design/divider"
+import { WrappedDividerProps } from "./interface"
+
+export const WrappedDivider: FC<WrappedDividerProps> = (props) => {
+  const { tooltipText, text, textAlign, textSize, hidden, color } = props
+
+  const _textSize = useMemo(() => {
+    return !isNaN(Number(textSize)) ? textSize + "px" : textSize?.toString()
+  }, [textSize])
+
+  return (
+    <TooltipWrapper
+      tooltipText={tooltipText}
+      disabled={!tooltipText}
+      position="tl"
+    >
+      <Wrapper w="500px" h="10px" alignment="fullWidth" hidden={hidden}>
+        <Divider
+          text={text}
+          textAlign={textAlign}
+          textSize={_textSize}
+          colorScheme={color}
+        />
+      </Wrapper>
+    </TooltipWrapper>
+  )
+}
+
+WrappedDivider.displayName = "WrappedDivider"
+
+export const DividerWidget = withParser(WrappedDivider)
---
+++ src/wrappedComponents/Divider/svg.tsx
@@ -0,0 +1,1 @@
+export const DividerIcon = () => <span>Divider</span>
