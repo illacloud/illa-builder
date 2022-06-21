3e566b5d765d2ca0cfb5476b6f2abf59638a3e7f
smallSohoSolo <b3069741@gmail.com>
2022-06-21 11:25:31 +0800

    feat: add displayname generator

--- src/utils/generators/generateDisplayName.ts
+++ src/utils/generators/generateDisplayName.ts
@@ -1,12 +1,26 @@
 // string for component
-export type DisplayNameType = "action" | "resource" | string
+import { ComponentNode } from "@/redux/currentApp/editor/components/componentsState"
 
 export class DisplayNameGenerator {
   static map: {
     [key: string]: number
   } = {}
+
+  static initialMap(componentNode: ComponentNode) {
+    if (
+      componentNode.type != null &&
+      new RegExp(`${componentNode.type}\\d+`).test(componentNode.displayName)
+    ) {
+      if (this.map.hasOwnProperty(componentNode.type)) {
+        this.map[componentNode.type] = this.map[componentNode.type] + 1
+      } else {
+        this.map[componentNode.type] = 1
+      }
+    }
+  }
+
   // use when create success
-  static getDisplayName(type: DisplayNameType): string {
+  static getDisplayName(type: string): string {
     if (this.map.hasOwnProperty(type)) {
       const num = this.map[type]
       this.map[type] = num + 1
