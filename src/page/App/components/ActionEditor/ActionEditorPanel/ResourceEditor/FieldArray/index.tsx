b296927843e5ee944b550f0afde6cb4998e8573a
Spike <l-yanlei@hotmail.com>
2022-06-24 13:43:50 +0800

    feat: complete `autoNewField` feature

--- src/page/App/components/ActionEditor/ActionEditorPanel/ResourceEditor/FieldArray/index.tsx
+++ src/page/App/components/ActionEditor/ActionEditorPanel/ResourceEditor/FieldArray/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from "react"
+import { FC, useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
 import { v4 as uuidv4 } from "uuid"
 import { PlusIcon, DeleteIcon } from "@illa-design/icon"
@@ -16,7 +16,7 @@ import {
 } from "./style"
 
 export const FieldArray: FC<FieldArrayProps> = (props) => {
-  const { hasType, onChange } = props
+  const { hasType, onChange, value, autoNewField } = props
 
   const { t } = useTranslation()
 
@@ -25,7 +25,24 @@ export const FieldArray: FC<FieldArrayProps> = (props) => {
       ? { key: "", type: "text", value: "", _key: uuidv4() }
       : { key: "", value: "", _key: uuidv4() }
   }
-  const [fields, setFields] = useState([getEmptyField()])
+  const [fields, setFields] = useState(
+    value?.length
+      ? value.map((v) => {
+        return { ...v, _key: uuidv4() }
+      })
+      : [getEmptyField()],
+  )
+
+  useEffect(() => {
+    setFields(
+      value?.length
+        ? value.map((v) => {
+          return { ...v, _key: uuidv4() }
+        })
+        : [getEmptyField()],
+    )
+  }, [value])
+
   const typeOptions = [
     { value: "text", label: "TEXT" },
     { value: "file", label: "File" },
@@ -43,55 +60,21 @@ export const FieldArray: FC<FieldArrayProps> = (props) => {
     onChange && onChange(fields.map(({ _key, ...rest }) => rest as ValueType))
   }
 
-  const fieldList = fields.map(({ type, _key }, index) => {
-    return (
-      <div css={fieldItemStyle} key={_key}>
-        {hasType ? (
-          <>
-            <CodeEditor
-              mode="TEXT_JS"
-              expectedType="String"
-              height="32px"
-              placeholder="key"
-              css={fieldItemKeyStyle}
-              onChange={(v) => updateField(index, "key", v)}
-            />
-            <Select
-              value={type}
-              options={typeOptions}
-              css={fieldItemTypeStyle}
-              size="small"
-              onChange={(v) => updateField(index, "type", v)}
-            />
-          </>
-        ) : (
-          <CodeEditor
-            mode="TEXT_JS"
-            expectedType="String"
-            height="32px"
-            placeholder="key"
-            css={fieldItemKeyStyle}
-            onChange={(v) => updateField(index, "key", v)}
-          />
-        )}
+  function autoAddField(index: number) {
+    if (!autoNewField) {
+      return
+    }
 
-        <CodeEditor
-          mode="TEXT_JS"
-          expectedType="String"
-          height="32px"
-          placeholder="value"
-          css={fieldItemValueStyle}
-          onChange={(v) => updateField(index, "value", v)}
-        />
-        <div css={deleteIconWrapperStyle} onClick={() => removeField(index)}>
-          <DeleteIcon size="12px" />
-        </div>
-      </div>
-    )
-  })
+    if (index === fields.length - 1) {
+      addField()
+    } else {
+      const { key, value } = fields[index]
+      key === "" && value === "" && removeField(index)
+    }
+  }
 
-  function addNewField() {
-    setFields([...fields, getEmptyField()])
+  function addField() {
+    setFields((preFields) => [...preFields, getEmptyField()])
   }
 
   function removeField(index: number) {
@@ -105,19 +88,83 @@ export const FieldArray: FC<FieldArrayProps> = (props) => {
     setFields(fieldsCopy)
   }
 
+  const fieldList = fields.map(({ type, _key }, index) => {
+    return (
+      <div css={fieldItemStyle} key={_key}>
+        {hasType ? (
+          <>
+            <CodeEditor
+              mode="TEXT_JS"
+              expectedType="String"
+              height="32px"
+              placeholder="key"
+              css={fieldItemKeyStyle}
+              value={fields[index]["key"]}
+              onChange={(v) => {
+                updateField(index, "key", v)
+                autoAddField(index)
+              }}
+            />
+            <Select
+              value={type}
+              options={typeOptions}
+              css={fieldItemTypeStyle}
+              size="small"
+              onChange={(v) => {
+                updateField(index, "type", v)
+                autoAddField(index)
+              }}
+            />
+          </>
+        ) : (
+          <CodeEditor
+            mode="TEXT_JS"
+            expectedType="String"
+            height="32px"
+            placeholder="key"
+            value={fields[index]["key"]}
+            css={fieldItemKeyStyle}
+            onChange={(v) => {
+              updateField(index, "key", v)
+              autoAddField(index)
+            }}
+          />
+        )}
+
+        <CodeEditor
+          mode="TEXT_JS"
+          expectedType="String"
+          height="32px"
+          placeholder="value"
+          value={fields[index]["value"]}
+          css={fieldItemValueStyle}
+          onChange={(v) => {
+            updateField(index, "value", v)
+            autoAddField(index)
+          }}
+        />
+        <div css={deleteIconWrapperStyle} onClick={() => removeField(index)}>
+          <DeleteIcon size="12px" />
+        </div>
+      </div>
+    )
+  })
+
   return (
     <div>
       {fieldList}
-      <Button
-        variant="text"
-        size="medium"
-        colorScheme="techPurple"
-        leftIcon={<PlusIcon />}
-        _css={newButtonStyle}
-        onClick={addNewField}
-      >
-        {t("editor.action.panel.btn.new")}
-      </Button>
+      {!autoNewField && (
+        <Button
+          variant="text"
+          size="medium"
+          colorScheme="techPurple"
+          leftIcon={<PlusIcon />}
+          _css={newButtonStyle}
+          onClick={addField}
+        >
+          {t("editor.action.panel.btn.new")}
+        </Button>
+      )}
     </div>
   )
 }
