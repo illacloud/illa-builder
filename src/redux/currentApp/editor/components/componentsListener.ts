21d0e97cd45c5f3880ce4774125f2f889d32cdcd
smallSohoSolo <b3069741@gmail.com>
2022-07-07 12:18:35 +0800

    fix: remove bring to front

--- src/redux/currentApp/editor/components/componentsListener.ts
+++ src/redux/currentApp/editor/components/componentsListener.ts
@@ -2,14 +2,13 @@ import { AppListenerEffectAPI, AppStartListening } from "@/store"
 import { Unsubscribe } from "@reduxjs/toolkit"
 import { componentsActions } from "@/redux/currentApp/editor/components/componentsSlice"
 import { displayNameActions } from "@/redux/currentApp/displayName/displayNameSlice"
+import { DisplayNameGenerator } from "@/utils/generators/generateDisplayName"
 
 async function handleDeleteExecution(
   action: ReturnType<typeof componentsActions.deleteComponentNodeReducer>,
   listenerApi: AppListenerEffectAPI,
 ) {
-  listenerApi.dispatch(
-    displayNameActions.removeDisplayNameReducer(action.payload.displayName),
-  )
+  DisplayNameGenerator.removeDisplayName(action.payload.displayName)
 }
 
 export function setupComponentsListeners(
