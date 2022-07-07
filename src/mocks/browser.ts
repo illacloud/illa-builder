8796ed599d6a8d9477dad1b992ff2686b7de231f
Jarvey <haojielin.linger@gmail.com>
2022-07-07 12:00:46 +0800

    chore: update design

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -5,7 +5,7 @@ import dashboard from "./apis/dashboard"
 import room from "./apis/room"
 import user from "./apis/user"
 
-const handlers = [...dashboard, ...room]
+const handlers = [...dashboard, ...room, ...action, ...resource, ...user]
 
 // This configures a Service Worker with the given request handlers.
 export const worker = setupWorker(...handlers)
