b152c990c0a13edba058662e4016b25d69ef2e64
smallSohoSolo <b3069741@gmail.com>
2022-07-11 20:27:25 +0800

    feat: add preview button new style

--- src/page/App/components/CanvasPanel/index.tsx
+++ src/page/App/components/CanvasPanel/index.tsx
@@ -25,6 +25,8 @@ export const CanvasPanel: FC<CanvasPanelProps> = (props) => {
       {mode === "edit" && (
         <Button
           css={previewStyle}
+          colorScheme="grayBlue"
+          variant="outline"
           leftIcon={<FullScreenIcon />}
           onClick={() => {
             dispatch(configActions.updateIllaMode("preview"))
