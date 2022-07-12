1700a8377f3ffc99c5d6dc44a2800823a0e52e75
AruSeito <weichen@illasoft.com>
2022-07-12 19:05:37 +0800

    fix: i18n

--- src/page/App/components/PanelSetters/MappedOptionSetter/index.tsx
+++ src/page/App/components/PanelSetters/MappedOptionSetter/index.tsx
@@ -2,14 +2,16 @@ import { FC } from "react"
 import { MappedOptionSetterProps } from "./interface"
 import { listStyle, listWrapperStyle, optionListHeaderStyle } from "./style"
 import { renderFieldAndLabel } from "@/page/App/components/InspectPanel/utils/fieldFactory"
+import { useTranslation } from "react-i18next"
 
 export const MappedOptionSetter: FC<MappedOptionSetterProps> = (props) => {
   const { attrName, labelName, childrenSetter, widgetDisplayName } = props
+  const { t } = useTranslation()
 
   return (
     <div css={listStyle}>
       <div css={optionListHeaderStyle}>
-        <div>{labelName}</div>
+        <div>{t(labelName)}</div>
       </div>
       <div css={listWrapperStyle}>
         {childrenSetter?.map((child) => {
