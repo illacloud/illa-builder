b004036ad355a09825dec4c993801e265f00f103
Spike <l-yanlei@hotmail.com>
2022-07-13 20:22:26 +0800

    refactor: remove no used code

--- src/utils/action/errorHandler.ts
+++ src/utils/action/errorHandler.ts
@@ -27,18 +27,6 @@ function handleSqlError(action: ActionItem, response: AxiosResponse) {
       }),
     )
   }
-  const errorPayload: ErrorShape = {
-    errorType: ExecutionErrorType.LINT,
-    errorMessage: errorData.message,
-    errorLine: errorData.lineNumber,
-  }
-
-  store.dispatch(
-    executionActions.updateExecutionErrorByAttrPathReducer({
-      attrPath: `${action.displayName}.query`,
-      value: errorPayload,
-    }),
-  )
 }
 
 export function errorHandler(action: ActionItem, response: AxiosResponse) {
