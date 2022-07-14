947d83015de498e96af019e4372f52c1bb9d6818
aoao <55166103+Z-AOAO@users.noreply.github.com>
2022-07-14 13:45:01 +0800

    feat: add errorPage

--- src/page/status/404/index.tsx
+++ src/page/status/404/index.tsx
@@ -1,5 +1,22 @@
 import { FC } from "react"
+import { ErrorPage } from "@/page/status/errorPage"
+import { Result404Icon } from "@illa-design/icon"
+import { buttonStyle, iconCss } from "@/page/status/style"
+import { Button } from "@illa-design/button"
+import { useNavigate } from "react-router-dom"
 
 export const Page404: FC = () => {
-  return <div>404</div>
+  const navigate = useNavigate()
+  return (
+    <ErrorPage
+      title={"404"}
+      des={"Whoops,that page is gone."}
+      img={<Result404Icon css={iconCss} />}
+    >
+      <div css={buttonStyle}>
+        <Button colorScheme={"gray"}>Again</Button>
+        <Button onClick={() => navigate(-1)}>back</Button>
+      </div>
+    </ErrorPage>
+  )
 }
--- src/page/status/500/index.tsx
+++ src/page/status/500/index.tsx
@@ -1,5 +1,21 @@
 import { FC } from "react"
+import { ErrorPage } from "@/page/status/errorPage"
+import { Result403Icon, Result500Icon } from "@illa-design/icon"
+import { buttonStyle, iconCss } from "@/page/status/style"
+import { Button } from "@illa-design/button"
+import { useNavigate } from "react-router-dom"
 
 export const Page500: FC = () => {
-  return <div>500</div>
+  const navigate = useNavigate()
+  return (
+    <ErrorPage
+      title={"500"}
+      des={"This page isn‘t working."}
+      img={<Result500Icon css={iconCss} />}
+    >
+      <div css={buttonStyle}>
+        <Button onClick={() => navigate(-1)}>back</Button>
+      </div>
+    </ErrorPage>
+  )
 }
