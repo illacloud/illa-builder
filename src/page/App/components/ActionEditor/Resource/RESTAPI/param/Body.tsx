5b8a186503a2909b16979e47707acf3479ec2b16
Spike <l-yanlei@hotmail.com>
2022-07-15 16:14:27 +0800

    feat: get rest api action body value

--- src/page/App/components/ActionEditor/Resource/RESTAPI/param/Body.tsx
+++ src/page/App/components/ActionEditor/Resource/RESTAPI/param/Body.tsx
@@ -1,59 +1,165 @@
-import { FC, useState } from "react"
+import { FC, useMemo, useCallback } from "react"
 import { Select } from "@illa-design/select"
-import { FieldArray } from "@/page/App/components/ActionEditor/ActionEditorPanel/ResourceEditor/FieldArray"
 import { CodeEditor } from "@/components/CodeEditor"
+import { FieldArray } from "@/page/App/components/ActionEditor/ActionEditorPanel/ResourceEditor/FieldArray"
+import {
+  initArrayField,
+  addArrayField,
+  removeArrayField,
+  updateArrayField,
+  wrappedWithKey,
+  getEmptyField,
+  excludeKeyAndEmptyFieldFromData,
+} from "@/page/App/components/ActionEditor/ActionEditorPanel/ResourceEditor/FieldArray/util"
 import { BodyParams, BodyProps, ContentType } from "../interface"
 import { bodyFieldStyle, descriptionCodeStyle, descriptionStyle } from "./style"
+import { ValueType } from "../../../ActionEditorPanel/ResourceEditor/FieldArray/interface"
+
+const bodyTypeOptiosn = [
+  {
+    value: "json",
+    label: "JSON",
+  },
+  {
+    value: "raw",
+    label: "Raw",
+  },
+  {
+    value: "x-www-form-urlencoded",
+    label: "x-www-form-urlencoded",
+  },
+  {
+    value: "form-data",
+    label: "Form Data",
+  },
+  {
+    value: "binary",
+    label: "binary",
+  },
+  {
+    value: "none",
+    label: "None",
+  },
+]
+
+const typeOptions = [
+  {
+    value: "text",
+    label: "Text",
+  },
+  {
+    value: "file",
+    label: "File",
+  },
+]
 
 export const Body: FC<BodyProps> = (props) => {
-  const { value, onChange } = props
-  const [bodyType, setBodyType] = useState<ContentType>("raw")
-  const bodyTypeOptiosn = [
-    {
-      value: "json",
-      label: "JSON",
-    },
-    {
-      value: "raw",
-      label: "Raw",
-    },
-    {
-      value: "x-www-form-urlencoded",
-      label: "x-www-form-urlencoded",
-    },
-    {
-      value: "form-data",
-      label: "Form Data",
-    },
-    {
-      value: "binary",
-      label: "binary",
-    },
-    {
-      value: "none",
-      label: "None",
-    },
-  ]
+  const { value, bodyType, onChangeBodyType, onChangeValue } = props
 
-  function renderBody() {
+  const onAddWithType = useCallback(() => {
+    onChangeValue?.([...(value as BodyParams[]), getEmptyField(true)])
+  }, [value, onChangeValue])
+
+  const onRemoveWithType = useCallback(
+    (_key) => {
+      const newVal = [...(value as BodyParams[])]
+
+      if ((value as BodyParams[]).length === 1) {
+        onChangeValue?.([getEmptyField(true)])
+        return
+      }
+
+      newVal.splice(
+        newVal.findIndex((i) => i._key === _key),
+        1,
+      )
+      onChangeValue?.(newVal)
+    },
+    [value, onChangeValue],
+  )
+
+  const onChangeWithType = useCallback(
+    (newValue) => {
+      const newVal = [...(value as BodyParams[])]
+
+      newVal.splice(
+        newVal.findIndex(({ _key }) => _key === newValue._key),
+        1,
+        newValue,
+      )
+
+      onChangeValue?.(newVal)
+    },
+    [value],
+  )
+
+  const onAdd = useCallback(() => {
+    onChangeValue?.([...value, getEmptyField()])
+  }, [value, onChangeValue])
+
+  const onRemove = useCallback(
+    (_key) => {
+      const newVal = [...value]
+
+      if (value.length === 1) {
+        onChangeValue?.([getEmptyField()])
+        return
+      }
+
+      newVal.splice(
+        newVal.findIndex((i) => i._key === _key),
+        1,
+      )
+      onChangeValue?.(newVal)
+    },
+    [value, onChangeValue],
+  )
+
+  const onChange = useCallback(
+    (newValue) => {
+      const newVal = [...value]
+
+      newVal.splice(
+        newVal.findIndex(({ _key }) => _key === newValue._key),
+        1,
+        newValue,
+      )
+
+      onChangeValue?.(newVal)
+    },
+    [value],
+  )
+
+  const bodyForm = useMemo(() => {
     switch (bodyType) {
       case "json":
       case "x-www-form-urlencoded":
-        return <FieldArray value={value as BodyParams[]} onChange={onChange} />
+        return (
+          <FieldArray
+            value={value as ValueType[]}
+            onAdd={onAdd}
+            onRemove={onRemove}
+            onChange={onChange}
+          />
+        )
       case "form-data":
         return (
           <FieldArray
             hasType
-            value={value as BodyParams[]}
-            onChange={onChange}
+            value={value as ValueType[]}
+            typeOptions={typeOptions}
+            onAdd={onAddWithType}
+            onRemove={onRemoveWithType}
+            onChange={onChangeWithType}
           />
         )
       case "raw":
         return (
           <CodeEditor
+            value={value as string}
             mode="TEXT_JS"
             expectedType="String"
-            onChange={onChange}
+            onChange={onChangeValue}
           />
         )
       case "binary":
@@ -66,9 +172,10 @@ export const Body: FC<BodyProps> = (props) => {
               >{`{data: binary string, filename?: string }`}</code>
             </dd>
             <CodeEditor
+              value={value as string}
               mode="TEXT_JS"
               expectedType="String"
-              onChange={onChange}
+              onChange={onChangeValue}
             />
           </>
         )
@@ -76,7 +183,29 @@ export const Body: FC<BodyProps> = (props) => {
       default:
         return null
     }
-  }
+  }, [bodyType, value, onChangeBodyType, onChangeValue])
+
+  const onBodyTypeChange = useCallback(
+    (bodyType: ContentType) => {
+      switch (bodyType) {
+        case "json":
+        case "x-www-form-urlencoded":
+          onChangeValue?.(initArrayField([getEmptyField(true)]))
+          break
+        case "form-data":
+          onChangeValue?.(initArrayField([getEmptyField()]))
+          break
+        case "raw":
+        case "binary":
+        default:
+          onChangeValue?.("")
+          break
+      }
+
+      onChangeBodyType?.(bodyType)
+    },
+    [onChangeBodyType, onChangeValue],
+  )
 
   return (
     <div css={bodyFieldStyle}>
@@ -84,10 +213,10 @@ export const Body: FC<BodyProps> = (props) => {
         colorScheme="techPurple"
         value={bodyType}
         options={bodyTypeOptiosn}
-        onChange={setBodyType}
-        size={"small"}
+        onChange={onBodyTypeChange}
+        size="medium"
       />
-      {renderBody()}
+      {bodyForm}
     </div>
   )
 }
