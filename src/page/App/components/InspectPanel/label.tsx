4c8fbb7893b3d208fadd97c632e716c5878d6b45
Spike <l-yanlei@hotmail.com>
2022-07-16 01:00:40 +0800

    feat: translate label tooltip

--- src/page/App/components/InspectPanel/label.tsx
+++ src/page/App/components/InspectPanel/label.tsx
@@ -1,14 +1,23 @@
 import { FC } from "react"
 import { Tooltip } from "@illa-design/tooltip"
-import { applyLabelTipsStyle } from "./style"
+import { useTranslation, Trans } from "react-i18next"
+import { applyLabelTipsStyle, labelTipsTextStyle } from "./style"
 import { PanelLabelProps } from "./interface"
 
 export const PanelLabel: FC<PanelLabelProps> = (props) => {
   const { labelDesc, labelName, isInList } = props
 
+  const { t } = useTranslation()
+
   return (
     <Tooltip
-      content={labelDesc}
+      content={
+        <span css={labelTipsTextStyle}>
+          <Trans style={{ color: "red", background: "red" }}>
+            {t(labelDesc)}
+          </Trans>
+        </span>
+      }
       trigger="hover"
       position="left"
       maxWidth="240px"
