2aa1a61361c12d312c5112df0d3d907aa3515785
Spike <l-yanlei@hotmail.com>
2022-07-15 18:58:30 +0800

    Revert "feat: stub"
    This reverts commit f20f365fa505f8a39dd80e6ecd21315b4eac82bd.

--- src/widgetLibrary/DropdownButtonWidget/eventHandlerConfig.ts
+++
@@ -1,6 +0,0 @@
-import { EventHandlerConfig } from "@/widgetLibrary/interface"
-
-export const DROPDOWN_BUTTON_EVENT_HANDLER_CONFIG: EventHandlerConfig = {
-  events: [],
-  methods: [],
-}
--- src/widgetLibrary/DropdownButtonWidget/index.tsx
+++
@@ -1,4 +0,0 @@
-export { DropdownButtonWidget, WrappedDropdownButton } from "./dropdownButton"
-export { DROPDOWN_BUTTON_WIDGET_CONFIG } from "./widgetConfig"
-export { DROPDOWN_BUTTON_PANEL_CONFIG } from "./panelConfig"
-export { DROPDOWN_BUTTON_EVENT_HANDLER_CONFIG } from "./eventHandlerConfig"
