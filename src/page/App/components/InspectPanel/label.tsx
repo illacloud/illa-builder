dbda394b80cb67799eced7d448b76ec70e159ea9
AruSeito <weichen@illasoft.com>
2022-07-22 16:08:33 +0800

    fix: i18n

--- src/page/App/components/InspectPanel/label.tsx
+++ src/page/App/components/InspectPanel/label.tsx
@@ -2,13 +2,21 @@ import { FC } from "react"
 import { Tooltip } from "@illa-design/tooltip"
 import { applyLabelTipsStyle, labelTipsTextStyle } from "./style"
 import { PanelLabelProps } from "./interface"
+import { globalColor, illaPrefix } from "@illa-design/theme"
+import { Text } from "@/widgetLibrary/TextWidget"
 
 export const PanelLabel: FC<PanelLabelProps> = (props) => {
   const { labelDesc, labelName, isInList } = props
 
   return (
     <Tooltip
-      content={<span css={labelTipsTextStyle}>{labelDesc}</span>}
+      content={
+        <Text
+          value={labelDesc}
+          disableMarkdown
+          textColor={globalColor(`--${illaPrefix}-white-01`)}
+        />
+      }
       trigger="hover"
       position="left"
       maxWidth="240px"
