52d0627041cec5b64de486d2ae764a014dd5db9b
AruSeito <weichen@illasoft.com>
2022-07-26 10:30:13 +0800

    fix: transform empty style

--- src/page/App/components/ActionEditor/ActionEditorPanel/ResourceEditor/Transformer/style.ts
+++ src/page/App/components/ActionEditor/ActionEditorPanel/ResourceEditor/Transformer/style.ts
@@ -4,7 +4,7 @@ import { globalColor, illaPrefix } from "@illa-design/theme"
 export const disableTransformerStyle = css`
   border-radius: 8px;
   background-color: ${globalColor(`--${illaPrefix}-grayBlue-09`)};
-  color: ${globalColor(`--${illaPrefix}-grayBlue-04`)};
+  color: ${globalColor(`--${illaPrefix}-grayBlue-03`)};
   padding: 8px 16px;
   font-size: 14px;
   line-height: 22px;
