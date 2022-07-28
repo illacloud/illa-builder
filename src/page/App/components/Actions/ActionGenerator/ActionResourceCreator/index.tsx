abba4a7914461cd4c123e8f14e7171c9491333d7
xiaoyu <lavamint@outlook.com>
2022-07-28 13:55:06 +0800

    feat: update api

--- src/page/App/components/Actions/ActionGenerator/ActionResourceCreator/index.tsx
+++ src/page/App/components/Actions/ActionGenerator/ActionResourceCreator/index.tsx
@@ -2,6 +2,14 @@ import { FC, useRef, useState } from "react"
 import { useSelector, useDispatch } from "react-redux"
 import { Button } from "@illa-design/button"
 import { PaginationPreIcon } from "@illa-design/icon"
+import { Notification } from "@illa-design/notification"
+import i18n from "@/i18n/config"
+import { Api } from "@/api/base"
+import { getActionList } from "@/redux/currentApp/action/actionSelector"
+import { MySQLConfigure } from "@/page/App/components/Actions/ActionGenerator/ActionResourceCreator/MySQLConfigure"
+import { RESTAPIConfigure } from "@/page/App/components/Actions/ActionGenerator/ActionResourceCreator/RestAPIConfigure"
+import { Resource, ResourceContent } from "@/redux/resource/resourceState"
+import { resourceActions } from "@/redux/resource/resourceSlice"
 import { ActionResourceCreatorProps, ConnectionRef } from "./interface"
 import {
   formContainerStyle,
@@ -12,10 +20,6 @@ import {
   formTitleStyle,
   formBodyStyle,
 } from "./style"
-import { getActionList } from "@/redux/currentApp/action/actionSelector"
-import i18n from "@/i18n/config"
-import { MySQLConfigure } from "@/page/App/components/Actions/ActionGenerator/ActionResourceCreator/MySQLConfigure"
-import { RESTAPIConfigure } from "@/page/App/components/Actions/ActionGenerator/ActionResourceCreator/RestAPIConfigure"
 
 export const ActionResourceCreator: FC<ActionResourceCreatorProps> = (
   props,
@@ -23,7 +27,7 @@ export const ActionResourceCreator: FC<ActionResourceCreatorProps> = (
   const {
     resourceId,
     onBack,
-    onSubmit,
+    onCreated,
     resourceType: resourceTypeProps,
   } = props
   const dispatch = useDispatch()
@@ -37,7 +41,7 @@ export const ActionResourceCreator: FC<ActionResourceCreatorProps> = (
   const formRef = useRef<HTMLFormElement>(null)
 
   const [createBtnLoading, setCreateBtnLoading] = useState(false)
-  const [testConnectionLoading, setTestConnectionLoading] = useState(false)
+  const [testConnectLoading, setTestConnectLoading] = useState(false)
 
   const resourceTitle = resourceType
     ? i18n.t(`editor.action.resource.${resourceType}.name`)
@@ -47,7 +51,71 @@ export const ActionResourceCreator: FC<ActionResourceCreatorProps> = (
     formRef.current?.requestSubmit()
   }
 
-  console.log({ resourceTitle }, resourceTypeProps, resource?.actionType)
+  function onTestConnection(data: any) {
+    Api.request<{ message: string }>(
+      {
+        url: "/resources/testConnection",
+        method: "POST",
+        data,
+      },
+      ({ data }) => {
+        Notification.success({ title: <span>{data.message}</span> })
+      },
+      ({ data }) => {
+        Notification.error({ title: <span>{data.errorMessage}</span> })
+      },
+      () => {},
+      (loading) => setTestConnectLoading(loading),
+    )
+  }
+
+  function addResourceItem(data: Partial<Resource<ResourceContent>>) {
+    Api.request<Resource<ResourceContent>>(
+      {
+        url: "/resources",
+        method: "POST",
+        data,
+      },
+      ({ data }) => {
+        dispatch(resourceActions.addResourceItemReducer(data))
+        onCreated?.(data.resourceId)
+      },
+      () => {},
+      () => {},
+      (loading) => setCreateBtnLoading(loading),
+    )
+  }
+
+  function updateResourceItem(
+    data: Partial<Resource<ResourceContent>>,
+    resourceId: string,
+  ) {
+    Api.request<Resource<ResourceContent>>(
+      {
+        url: `/resources/${resourceId}`,
+        method: "PUT",
+        data,
+      },
+      ({ data }) => {
+        dispatch(resourceActions.updateResourceItemReducer(data))
+        onCreated?.(resourceId)
+      },
+      () => {},
+      () => {},
+      (loading) => setCreateBtnLoading(loading),
+    )
+  }
+
+  function onSubmitForm(
+    data: Partial<Resource<ResourceContent>>,
+    resourceId?: string,
+  ) {
+    if (resourceId) {
+      updateResourceItem(data, resourceId)
+    } else {
+      addResourceItem(data)
+    }
+  }
 
   return (
     <div css={formContainerStyle}>
@@ -57,17 +125,24 @@ export const ActionResourceCreator: FC<ActionResourceCreatorProps> = (
       <div css={formBodyStyle}>
         {resourceType === "mysql" ? (
           <MySQLConfigure
+            ref={formRef}
             connectionRef={connectionRef}
             resourceId={resourceId}
+            onTestConnection={onTestConnection}
+            onSubmit={(data) => onSubmitForm(data, resourceId)}
           />
         ) : resourceType === "restapi" ? (
-          <RESTAPIConfigure />
+          <RESTAPIConfigure
+            ref={formRef}
+            resourceId={resourceId}
+            onSubmit={(data) => onSubmitForm(data, resourceId)}
+          />
         ) : null}
         {/*{renderResourceNode(*/}
         {/*    resourceType,*/}
         {/*    connectionRef,*/}
         {/*    formRef,*/}
-        {/*    onSubmitForm,*/}
+        {/*    onCreatedForm,*/}
         {/*    onTestConnection,*/}
         {/*    props,*/}
         {/*)}*/}
@@ -95,7 +170,7 @@ export const ActionResourceCreator: FC<ActionResourceCreatorProps> = (
           onClick={() => {
             connectionRef.current?.testConnection()
           }}
-          loading={testConnectionLoading}
+          loading={testConnectLoading}
         >
           {i18n.t("editor.action.form.btn.test_connection")}
         </Button>
