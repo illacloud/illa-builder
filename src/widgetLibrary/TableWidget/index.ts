caadc99e81f0afa1f5149c4b45970b1684e1be6e
AruSeito <weichen@illasoft.com>
2022-07-28 18:22:23 +0800

    feat: base table

---
+++ src/widgetLibrary/TableWidget/index.ts
@@ -0,0 +1,5 @@
+import { TABLE_WIDGET_CONFIG } from "@/widgetLibrary/TableWidget/widgetConfig"
+
+export { TABLE_WIDGET_CONFIG } from "./widgetConfig"
+export { TABLE_PANEL_CONFIG } from "./panelConfig"
+export { TableWidget } from "./table"
