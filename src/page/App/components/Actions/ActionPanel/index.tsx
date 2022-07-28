b58a693e555f9513c6f6ccd7a20b2fb298e4769f
xiaoyu-a <lavamint@outlook.com>
2022-07-29 01:28:23 +0800

    feat: add copy and delete

--- src/page/App/components/Actions/ActionPanel/index.tsx
+++ src/page/App/components/Actions/ActionPanel/index.tsx
@@ -1,20 +1,34 @@
 import { FC, ReactNode } from "react"
 import { actionPanelStyle } from "@/page/App/components/Actions/ActionPanel/style"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { getSelectedAction } from "@/redux/config/configSelector"
 import { ActionTitleBar } from "@/page/App/components/Actions/ActionPanel/ActionTitleBar"
 import { MysqlPanel } from "./MysqlPanel"
 import { RestApiPanel } from "@/page/App/components/Actions/ActionPanel/RestApiPanel"
 import { TransformerPanel } from "@/page/App/components/Actions/ActionPanel/TransformerPanel"
-import { ActionItem } from "@/redux/currentApp/action/actionState"
+import {
+  ActionContent,
+  ActionItem,
+} from "@/redux/currentApp/action/actionState"
 import { MysqlAction } from "@/redux/currentApp/action/mysqlAction"
 import {
   BodyContent,
   RestApiAction,
 } from "@/redux/currentApp/action/restapiAction"
 import { TransformerAction } from "@/redux/currentApp/action/transformerAction"
+import { DisplayNameGenerator } from "@/utils/generators/generateDisplayName"
+import { Api } from "@/api/base"
+import { Message } from "@illa-design/message"
+import i18n from "@/i18n/config"
+import { actionActions } from "@/redux/currentApp/action/actionSlice"
+import { configActions } from "@/redux/config/configSlice"
+import { useParams } from "react-router-dom"
+import { omit } from "@illa-design/system"
 
 export const ActionPanel: FC = () => {
+  const dispatch = useDispatch()
+  const params = useParams()
+  const baseActionUrl = `/apps/${params.appId}/actions`
   const selectedAction = useSelector(getSelectedAction)
   // null selected
   if (selectedAction === null || selectedAction === undefined) {
@@ -50,12 +64,65 @@ export const ActionPanel: FC = () => {
     default:
       break
   }
+
+  function onCopyActionItem(action: ActionItem<ActionContent>) {
+    const newAction = omit(action, ["resourceId"])
+    const displayName = DisplayNameGenerator.getDisplayName(action.actionType)
+    const data: Partial<ActionItem<{}>> = {
+      ...newAction,
+      displayName,
+    }
+    Api.request(
+      {
+        url: baseActionUrl,
+        method: "POST",
+        data,
+      },
+      ({ data }: { data: ActionItem<ActionContent> }) => {
+        Message.success(
+          i18n.t("editor.action.action_list.message.success_created"),
+        )
+        dispatch(actionActions.addActionItemReducer(data))
+        dispatch(configActions.updateSelectedAction(data))
+      },
+      () => {
+        Message.error(i18n.t("dashboard.resources.trash_success"))
+        DisplayNameGenerator.removeDisplayName(displayName)
+      },
+      () => {},
+      (loading) => {},
+    )
+  }
+
+  function onDeleteActionItem(action: ActionItem<ActionContent>) {
+    console.log("onDeleteActionItem", action)
+
+    const { actionId } = action
+    Api.request(
+      {
+        url: `${baseActionUrl}/${actionId}`,
+        method: "DELETE",
+      },
+      ({ data }: { data: ActionItem<ActionContent> }) => {
+        const { displayName } = data
+        DisplayNameGenerator.removeDisplayName(displayName)
+        dispatch(actionActions.removeActionItemReducer(displayName))
+        Message.error(i18n.t("dashboard.resources.trash_success"))
+      },
+      () => {
+        Message.error(i18n.t("editor.action.action_list.message.failed"))
+      },
+      () => {},
+      (loading) => {},
+    )
+  }
+
   return (
     <div css={actionPanelStyle}>
       <ActionTitleBar
         action={selectedAction}
-        onCopy={() => {}}
-        onDelete={() => {}}
+        onCopy={onCopyActionItem}
+        onDelete={onDeleteActionItem}
       />
       {actionPanel}
     </div>
