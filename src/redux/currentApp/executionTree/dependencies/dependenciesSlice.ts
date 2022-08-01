3b1ef72e7e4334a1d517ac5ba8b3032c4a206df5
AruSeito <weichen@illasoft.com>
2022-08-01 14:39:20 +0800

    fix: action run result

--- src/redux/currentApp/executionTree/dependencies/dependenciesSlice.ts
+++ src/redux/currentApp/executionTree/dependencies/dependenciesSlice.ts
@@ -1,13 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 import { dependenciesInitialState } from "@/redux/currentApp/executionTree/dependencies/dependenciesState"
-import { setDependenciesReducer } from "@/redux/currentApp/executionTree/dependencies/dependenciesReducer"
+import {
+  setDependenciesReducer,
+  startCalcReducer,
+} from "@/redux/currentApp/executionTree/dependencies/dependenciesReducer"
 
 const dependenciesSlice = createSlice({
   name: "dependencies",
   initialState: dependenciesInitialState,
   reducers: {
     setDependenciesReducer,
+    startCalcReducer,
   },
 })
 
