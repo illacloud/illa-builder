b8a0cd6b21c43fc3d652183bac9a39f48b160eda
smallSohoSolo <b3069741@gmail.com>
2022-08-04 01:16:38 +0800

    fix: test

--- src/widgetLibrary/PublicSector/Label/tests/label.test.tsx
+++ src/widgetLibrary/PublicSector/Label/tests/label.test.tsx
@@ -17,10 +17,3 @@ test("Render with required", () => {
   render(<Label label="testLabel" required />)
   expect(screen.getByText("*")).toBeInTheDocument()
 })
-
-test("Render with labelAlign and labelPosition", () => {
-  render(<Label label="testLabel" labelAlign="right" labelPosition="top" />)
-  const labelWrapper = screen.getByText("testLabel").parentElement
-  expect(labelWrapper).toHaveStyle("text-align: right")
-  expect(labelWrapper).toHaveStyle("margin-bottom: 8px")
-})
