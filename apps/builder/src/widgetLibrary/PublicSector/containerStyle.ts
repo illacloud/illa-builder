fc29a2b7615f69f07a12636afb9b3fc0a2c506f0
AruSeito <weichen@illasoft.com>
2022-08-05 15:32:51 +0800

    fix: position error

--- apps/builder/src/widgetLibrary/PublicSector/containerStyle.ts
+++ apps/builder/src/widgetLibrary/PublicSector/containerStyle.ts
@@ -2,8 +2,7 @@ import { css } from "@emotion/react"
 
 export const containerStyle = css`
   display: flex;
-  flex-direction: column;
   width: 100%;
   align-items: center;
-  justify-content: center;
+  justify-content: end;
 `
