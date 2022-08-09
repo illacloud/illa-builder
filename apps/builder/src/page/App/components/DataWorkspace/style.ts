2d5b1076aae98b0d700caafd4cccfe1a25a33e91
smallSohoSolo <b3069741@gmail.com>
2022-08-09 16:36:07 +0800

    chore: remove unused style

--- apps/builder/src/page/App/components/DataWorkspace/style.ts
+++ apps/builder/src/page/App/components/DataWorkspace/style.ts
@@ -1,31 +1,4 @@
 import { css, SerializedStyles } from "@emotion/react"
-import { globalColor, illaPrefix } from "@illa-design/theme"
-
-export const itemNameStyle: SerializedStyles = css`
-  font-size: 12px;
-  font-weight: 500;
-`
-
-export const itemNameDescStyle: SerializedStyles = css`
-  font-size: 12px;
-  color: ${globalColor(`--${illaPrefix}-grayBlue-04`)};
-`
-
-export const jsonNameStyle: SerializedStyles = css`
-  display: inline-block;
-  font-size: 12px;
-  color: ${globalColor(`--${illaPrefix}-grayBlue-02`)};
-`
-
-export const jsonValueStyle: SerializedStyles = css`
-  display: inline-block;
-  font-size: 12px;
-`
-
-export const jsonItemStyle: SerializedStyles = css`
-  overflow-wrap: break-word;
-  word-break: break-all;
-`
 
 export function applyJsonValueColorStyle(type: any): SerializedStyles {
   switch (type) {
