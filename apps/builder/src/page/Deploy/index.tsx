486b7d4ce4d473742679dff40618ba9af7f45f7f
AruSeito <weichen@illasoft.com>
2022-08-30 14:15:51 +0800

    fix: deploy bug

--- apps/builder/src/page/Deploy/index.tsx
+++ apps/builder/src/page/Deploy/index.tsx
@@ -1,6 +1,10 @@
 import { FC, useEffect } from "react"
 import { CanvasPanel } from "@/page/App/components/CanvasPanel"
-import { deployContainerStyle, deployLogoStyle } from "@/page/Deploy/style"
+import {
+  deployContainerStyle,
+  deployLogoStyle,
+  logoStyle,
+} from "@/page/Deploy/style"
 import { Loading } from "@illa-design/loading"
 import { useInitBuilderApp } from "@/hooks/useInitApp"
 import { Unsubscribe } from "@reduxjs/toolkit"
@@ -33,12 +37,13 @@ export const Deploy: FC = () => {
         variant="fill"
         size="large"
         bd={`solid 1px ${globalColor(`--${illaPrefix}-grayBlue-09`)}`}
-        rightIcon={<Logo />}
+        h="40px"
+        rightIcon={<Logo css={logoStyle} />}
         onClick={() => {
           window.open("https://illacloud.com", "_blank")
         }}
       >
-        <span style={{ fontWeight: "500" }}>Powered by</span>
+        <span style={{ fontWeight: "500", fontSize: "12px" }}>Powered by</span>
       </Button>
     </div>
   )
--- apps/builder/src/page/Deploy/style.ts
+++ apps/builder/src/page/Deploy/style.ts
@@ -1,7 +1,6 @@
 import { globalColor, illaPrefix } from "@illa-design/theme"
 import { css } from "@emotion/react"
 
-// TODO: @aruseito change canvas position and zindex
 export const deployContainerStyle = css`
   width: 100%;
   height: 100vh;
@@ -12,4 +11,7 @@ export const deployLogoStyle = css`
   bottom: 16px;
   right: 16px;
   box-shadow: 0 0 8px 0 ${globalColor(`--${illaPrefix}-blackAlpha-07`)};
+`
+export const logoStyle = css`
+  height: 12px;
 `
