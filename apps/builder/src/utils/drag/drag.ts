56016749e98db71cfbcdb01a7b3445bcd8e29f47
AruSeito <weichen@illasoft.com>
2022-09-08 17:22:23 +0800

    perf: improve drag

--- apps/builder/src/utils/drag/drag.ts
+++ apps/builder/src/utils/drag/drag.ts
@@ -1,49 +1,31 @@
 import { ComponentNode } from "@/redux/currentApp/editor/components/componentsState"
 import { configActions } from "@/redux/config/configSlice"
 import { componentsActions } from "@/redux/currentApp/editor/components/componentsSlice"
-import {
-  getCanvas,
-  searchDsl,
-} from "@/redux/currentApp/editor/components/componentsSelector"
-import store, { RootState } from "@/store"
-import { dottedLineSquareActions } from "@/redux/currentApp/editor/dottedLineSquare/dottedLineSquareSlice"
-import { dragShadowActions } from "@/redux/currentApp/editor/dragShadow/dragShadowSlice"
+import store from "@/store"
 import { DisplayNameGenerator } from "@/utils/generators/generateDisplayName"
 
-export function startDrag(dragNode: ComponentNode) {
+export function startDrag(dragNode: ComponentNode, isAdd: boolean = false) {
   store.dispatch(configActions.updateShowDot(true))
-  store.dispatch(
-    componentsActions.updateComponentsShape({
-      isMove: true,
-      components: [
-        {
-          ...dragNode,
-          isDragging: true,
-        },
-      ],
-    }),
-  )
+  if (!isAdd) {
+    store.dispatch(
+      componentsActions.updateComponentsShape({
+        isMove: true,
+        components: [
+          {
+            ...dragNode,
+            isDragging: true,
+          },
+        ],
+      }),
+    )
+  }
 }
 
 export function endDrag(dragNode: ComponentNode, isDropOnCanvas: boolean) {
   store.dispatch(configActions.updateShowDot(false))
   if (isDropOnCanvas) {
     store.dispatch(configActions.updateSelectedComponent([dragNode]))
-  }
-  // remove dotted line square
-  store.dispatch(
-    dottedLineSquareActions.removeDottedLineSquareReducer(dragNode.displayName),
-  )
-  // remove drag
-  store.dispatch(
-    dragShadowActions.removeDragShadowReducer(dragNode.displayName),
-  )
-  if (
-    searchDsl(
-      store.getState().currentApp.editor.components,
-      dragNode.displayName,
-    ) == null
-  ) {
+  } else {
     DisplayNameGenerator.removeDisplayName(dragNode.displayName)
   }
 }
