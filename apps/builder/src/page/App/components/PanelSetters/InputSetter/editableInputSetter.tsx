6c5b2b9b30d6ef5ee92bd20f26dbbe13f4002bea
AruSeito <weichen@illasoft.com>
2022-10-09 17:54:55 +0800

    fix: icon style

--- apps/builder/src/page/App/components/PanelSetters/InputSetter/editableInputSetter.tsx
+++ apps/builder/src/page/App/components/PanelSetters/InputSetter/editableInputSetter.tsx
@@ -1,13 +1,16 @@
 import { FC } from "react"
 import { Input } from "@illa-design/input"
-import { editableInputSetterStyle } from "@/page/App/components/PanelSetters/InputSetter/style"
+import {
+  editableInputIconStyle,
+  editableInputSetterStyle,
+} from "@/page/App/components/PanelSetters/InputSetter/style"
 import { EditableInputSetterProps } from "@/page/App/components/PanelSetters/InputSetter/interface"
 
 export const EditableInputSetter: FC<EditableInputSetterProps> = (props) => {
   const { value, handleUpdateDsl, attrName, icon } = props
   return (
     <div css={editableInputSetterStyle}>
-      {icon ? icon : null}
+      {icon ? <div css={editableInputIconStyle}>{icon}</div> : null}
       <div style={{ width: "130px" }}>
         <Input
           withoutNormalBorder
