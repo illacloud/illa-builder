f9e72f2c88d0d5caa8094df7a79ae76b32710604
AruSeito <weichen@illasoft.com>
2022-11-16 16:26:05 +0800

    build: update vite config

--- apps/builder/vite.config.ts
+++ apps/builder/vite.config.ts
@@ -45,6 +45,7 @@ export default defineConfig((props) => {
       alias: {
         "@": resolve(__dirname, "src"),
         "@assets": resolve(__dirname, "src/assets"),
+        "@illa-design": resolve(__dirname, "../../illa-design/packages"),
       },
     },
     build: {
