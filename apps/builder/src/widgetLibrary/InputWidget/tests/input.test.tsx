475a411f1b7624a4c429198737fb947fba66f3f4
smallSohoSolo <b3069741@gmail.com>
2022-12-06 17:59:45 +0800

    refactor: cypress version

--- apps/builder/src/widgetLibrary/InputWidget/tests/input.test.tsx
+++
@@ -1,7 +0,0 @@
-import "@testing-library/jest-dom"
-import { render, screen } from "@testing-library/react"
-
-test("WrappedInput renders correctly", () => {
-  render(<div>WrappedInput</div>)
-  expect(screen.getByText("WrappedInput")).toBeInTheDocument()
-})
