78e389a2c3fdea6ea09929754244ad584489e4aa
AruSeito <weichen@illasoft.com>
2023-01-16 00:28:53 +0800

    fix: i18n type error

--- apps/builder/src/i18n/i18next.d.ts
+++ apps/builder/src/i18n/i18next.d.ts
@@ -1,7 +1,7 @@
-import "react-i18next"
+import "i18next"
 import { resources } from "./config"
 
-declare module "react-i18next" {
+declare module "i18next" {
   interface CustomTypeOptions {
     resources: typeof resources["en-US"]
     returnNull: false
--- apps/builder/src/page/App/components/Actions/ActionPanel/HuggingFacePanel/index.tsx
+++ apps/builder/src/page/App/components/Actions/ActionPanel/HuggingFacePanel/index.tsx
@@ -178,16 +178,6 @@ export const HuggingFacePanel: FC = () => {
   const dispatch = useDispatch()
 
   const handleURLClick = (link: string) => window.open(link, "_blank")
-  const getTransComponent = (key: string, link: string) => {
-    const handleLinKClick = () => handleURLClick(link)
-    return (
-      <Trans
-        i18nKey={key}
-        t={t}
-        components={[<TextLink key={key} onClick={handleLinKClick} />]}
-      />
-    )
-  }
 
   const handleValueChange = useCallback(
     (value: string | boolean, key: string) => {
@@ -315,10 +305,22 @@ export const HuggingFacePanel: FC = () => {
           value={content.modelID ?? ""}
           onChange={(value) => handleValueChange(value, "modelID")}
           expectedType={VALIDATION_TYPES.STRING}
-          tips={getTransComponent(
-            "editor.action.panel.hugging_face.tips.mode_id",
-            "https://huggingface.co/docs/api-inference/detailed_parameters",
-          )}
+          tips={
+            <Trans
+              i18nKey="editor.action.panel.hugging_face.tips.mode_id"
+              t={t}
+              components={[
+                <TextLink
+                  key="editor.action.panel.hugging_face.tips.mode_id"
+                  onClick={() => {
+                    handleURLClick(
+                      "https://huggingface.co/docs/api-inference/detailed_parameters",
+                    )
+                  }}
+                />,
+              ]}
+            />
+          }
         />
         <div css={bodyEditorContainerStyle}>
           <span css={bodyLabelStyle}>
@@ -374,7 +376,7 @@ export const HuggingFacePanel: FC = () => {
             )}
             {currentParameterType === "text" && (
               <CodeEditor
-                css={textCodeEditorStyle}
+                wrapperCss={textCodeEditorStyle}
                 lang={CODE_LANG.JAVASCRIPT}
                 value={(content?.inputs.content ?? "") as TextRawBody}
                 onChange={handleInputsValueChange}
@@ -408,10 +410,20 @@ export const HuggingFacePanel: FC = () => {
                   </span>
                 </div>
                 <div css={checkboxTipsStyle}>
-                  {getTransComponent(
-                    "editor.action.panel.hugging_face.tips.use_detail_parameters",
-                    "https://huggingface.co/docs/api-inference/detailed_parameters",
-                  )}
+                  <Trans
+                    i18nKey="editor.action.panel.hugging_face.tips.use_detail_parameters"
+                    t={t}
+                    components={[
+                      <TextLink
+                        key="editor.action.panel.hugging_face.tips.use_detail_parameters"
+                        onClick={() => {
+                          handleURLClick(
+                            "https://huggingface.co/docs/api-inference/detailed_parameters",
+                          )
+                        }}
+                      />,
+                    ]}
+                  />
                 </div>
               </div>
             </div>
