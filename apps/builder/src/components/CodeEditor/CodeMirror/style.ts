684abf64282bde4416f321dc42294fa4e66c8def
AruSeito <weichen@illasoft.com>
2023-01-17 19:55:53 +0800

    fix: codeMirror theme

--- apps/builder/src/components/CodeEditor/CodeMirror/style.ts
+++ apps/builder/src/components/CodeEditor/CodeMirror/style.ts
@@ -27,6 +27,7 @@ export const applyEditorWrapperStyle = (
           :hover {
             border: 1px solid
               ${getEditorWrapperBorderColor(hasError, isFocused, true)};
+            z-index: 1;
           }
         `
   return css`
@@ -35,6 +36,9 @@ export const applyEditorWrapperStyle = (
       border: 1px solid
         ${getEditorWrapperBorderColor(hasError, isFocused, false)};
       border-radius: ${isFocused ? "8px 8px 0px 0px;" : "8px"};
+      &.cm-focused {
+        z-index: 1;
+      }
       ${hoverStyle}
     }
   `
