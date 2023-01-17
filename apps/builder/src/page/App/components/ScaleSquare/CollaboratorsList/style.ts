ba7cc24bbf3463794f07a398072b61c8ed57ec3e
shandamengcheng <zhumengcheng@illasoft.com>
2023-01-17 18:36:05 +0800

    fix: delete useless style

--- apps/builder/src/page/App/components/ScaleSquare/CollaboratorsList/style.ts
+++ apps/builder/src/page/App/components/ScaleSquare/CollaboratorsList/style.ts
@@ -50,8 +50,6 @@ export const getComponentUsersListContainerStyle = (
     flex-grow: 1;
     justify-content: flex-start;
     align-self: center;
-    position: relative;
-    z-index: 10;
     overflow: hidden;
     min-width: ${minWidth}px;
   `
