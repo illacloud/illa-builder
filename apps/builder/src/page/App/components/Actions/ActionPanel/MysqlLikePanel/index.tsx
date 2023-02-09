9e97970fc898aed0f94fcffe69374ff001a5130d
smallSohoSolo <b3069741@gmail.com>
2023-02-09 16:50:15 +0800

    feat: add open ai style

--- apps/builder/src/page/App/components/Actions/ActionPanel/MysqlLikePanel/index.tsx
+++ apps/builder/src/page/App/components/Actions/ActionPanel/MysqlLikePanel/index.tsx
@@ -1,7 +1,9 @@
-import { FC, useEffect, useMemo, useState } from "react"
+import { FC, useEffect, useMemo, useRef, useState } from "react"
+import { useTranslation } from "react-i18next"
 import { useDispatch, useSelector } from "react-redux"
-import { isObject } from "@illa-design/react"
+import { Button, Input } from "@illa-design/react"
 import { Api } from "@/api/base"
+import { ReactComponent as OpenAIIcon } from "@/assets/openai.svg"
 import { CodeEditor } from "@/components/CodeEditor"
 import { CODE_LANG } from "@/components/CodeEditor/CodeMirror/extensions/interface"
 import { ActionEventHandler } from "@/page/App/components/Actions/ActionPanel/ActionEventHandler"
@@ -9,6 +11,7 @@ import {
   actionItemContainer,
   mysqlContainerStyle,
   sqlInputStyle,
+  sqlTransStyle,
 } from "@/page/App/components/Actions/ActionPanel/MysqlLikePanel/style"
 import { ResourceChoose } from "@/page/App/components/Actions/ActionPanel/ResourceChoose"
 import { TransformerComponent } from "@/page/App/components/Actions/ActionPanel/TransformerComponent"
@@ -22,6 +25,8 @@ export const MysqlLikePanel: FC = (props) => {
   const currentAction = useSelector(getCachedAction)!!
   const [sqlTable, setSqlTable] = useState<Record<string, unknown>>()
   const dispatch = useDispatch()
+
+  const { t } = useTranslation()
 
   useEffect(() => {
     Api.request(
@@ -52,10 +57,31 @@ export const MysqlLikePanel: FC = (props) => {
     return (currentAction.content as MysqlLikeAction)?.query || ""
   }, [currentAction])
 
+  const inputRef = useRef<HTMLInputElement>(null)
+
   return (
     <div css={mysqlContainerStyle}>
       <ResourceChoose />
       <div css={actionItemContainer}>
+        <div css={sqlTransStyle}>
+          <Input
+            size="large"
+            colorScheme="techPurple"
+            bdRadius="8px 0 0 8px"
+            flexGrow="1"
+            flexShrink="1"
+            placeholder={"place"}
+            inputRef={inputRef}
+          />
+          <Button
+            size="large"
+            bdRadius="0 8px 8px 0"
+            bg="linear-gradient(90deg, #FF53D9 0%, #AE47FF 100%);"
+            leftIcon={<OpenAIIcon />}
+          >
+            {"test test test"}
+          </Button>
+        </div>
         <div css={sqlInputStyle}>
           <CodeEditor
             placeholder="select * from users;"
