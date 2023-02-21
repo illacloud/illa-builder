8f988e6b8bae7ddd4240fd83971a1d06bcbdd150
AruSeito <weichen@illasoft.com>
2023-02-21 15:12:07 +0800

    fix: icons group data

--- apps/builder/src/widgetLibrary/IconWidget/utils.ts
+++ apps/builder/src/widgetLibrary/IconWidget/utils.ts
@@ -1,15 +1,19 @@
-import * as RiLib from "react-icons"
-import * as ai from "react-icons/ai"
-import * as bi from "react-icons/bi"
+import * as RiLib from "react-icons/"
 import * as bs from "react-icons/bs"
+import * as fc from "react-icons/fc"
+import * as sl from "react-icons/sl"
 import * as tb from "react-icons/tb"
 import { RIALL } from "./interface"
 
-export const ALL_ICONS = (RiLib as unknown as RIALL)["IconsManifest"]
-
+const IMPORT_ICONS_ID = ["sl", "fc", "bs", "tb"]
+export const ALL_ICONS = (RiLib as unknown as RIALL)["IconsManifest"].filter(
+  (iconManifest) => {
+    return IMPORT_ICONS_ID.includes(iconManifest.id)
+  },
+)
 export const AllIconData: Record<string, Record<string, RiLib.IconType>> = {
-  ai,
-  bi,
+  sl,
+  fc,
   bs,
   tb,
 }
