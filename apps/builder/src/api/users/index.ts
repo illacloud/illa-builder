a5a800ca66abd3fcbe7e07683279633271df71ed
xiaoyu-a <lavamint@outlook.com>
2023-02-21 23:53:02 +0800

    feat: update upload api header

--- apps/builder/src/api/users/index.ts
+++ apps/builder/src/api/users/index.ts
@@ -9,7 +9,6 @@ export interface UploadResponse {
 
 export const upload = async (url: string, file: Blob) => {
   const resUrl = url.split("?")[0]
-  const length = file.size
 
   await Api.asyncRequest({
     url,
@@ -17,8 +16,8 @@ export const upload = async (url: string, file: Blob) => {
     data: file,
     headers: {
       "Content-Type": "multipart/form-data",
+      "Content-Encoding": "compress",
       "x-amz-acl": "public-read",
-      "content-length": length,
     },
   })
   return resUrl
