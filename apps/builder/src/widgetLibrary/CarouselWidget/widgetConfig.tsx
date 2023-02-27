47ef505b9ca0eae11b7a486bbebdb4ae1bdb11e0
xiaoyu <lavamint@outlook.com>
2023-02-27 19:44:45 +0800

    feat: add manual data

--- apps/builder/src/widgetLibrary/CarouselWidget/widgetConfig.tsx
+++ apps/builder/src/widgetLibrary/CarouselWidget/widgetConfig.tsx
@@ -1,3 +1,4 @@
+import { v4 } from "uuid"
 import { ReactComponent as CarouselWidgetIcon } from "@/assets/widgetCover/carousel.svg"
 import i18n from "@/i18n/config"
 import { WidgetConfig } from "@/widgetLibrary/interface"
@@ -15,5 +16,23 @@ export const CAROUSEL_WIDGET_CONFIG: WidgetConfig = {
     showArrows: true,
     showDots: true,
     hidden: false,
+    manualData: [
+      {
+        id: `img-${v4()}`,
+        value: "Image 1",
+        url: "https://images.unsplash.com/photo-1614853316476-de00d14cb1fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
+      },
+      {
+        id: `img-${v4()}`,
+        value: "Image 2",
+        url: "https://images.unsplash.com/photo-1614853316476-de00d14cb1fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
+      },
+      {
+        id: `img-${v4()}`,
+        value: "Image 3",
+        url: "https://images.unsplash.com/photo-1614853316476-de00d14cb1fc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
+      },
+    ],
+    dataSources: "{{[]}}",
   },
 }
