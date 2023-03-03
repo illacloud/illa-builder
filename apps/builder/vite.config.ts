76debdfb024fb2a7e19305a84999c200e75e0a1d
xiaoyu <lavamint@outlook.com>
2023-03-03 11:19:12 +0800

    fix: clear useless code

--- apps/builder/vite.config.ts
+++ apps/builder/vite.config.ts
@@ -7,8 +7,6 @@ import { defineConfig, loadEnv } from "vite"
 import checker from "vite-plugin-checker"
 import svgr from "vite-plugin-svgr"
 
-// const mdx = require("@mdx-js/rollup")
-
 // https://vitejs.dev/config/
 export default defineConfig((props) => {
   const env = loadEnv(props.mode, process.cwd(), "")
