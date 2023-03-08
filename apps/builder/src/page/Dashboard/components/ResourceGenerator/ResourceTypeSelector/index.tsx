2f7001df3f3e83d8d48011415692b4d9670604a3
shandamengcheng <zhumengcheng@illasoft.com>
2023-03-08 20:24:13 +0800

    feat: add resource list  whitelist

--- apps/builder/src/page/Dashboard/components/ResourceGenerator/ResourceTypeSelector/index.tsx
+++ apps/builder/src/page/Dashboard/components/ResourceGenerator/ResourceTypeSelector/index.tsx
@@ -1,11 +1,46 @@
-import { FC } from "react"
+import copy from "copy-to-clipboard"
+import { FC, useCallback, useState } from "react"
+import { useTranslation } from "react-i18next"
+import { CopyIcon, DownIcon, UpIcon, createMessage } from "@illa-design/react"
 import { ResourceCard } from "@/page/Dashboard/components/ResourceGenerator/ResourceCard"
 import { ResourceTypeList } from "@/page/Dashboard/components/ResourceGenerator/config"
+import { isCloudVersion } from "@/utils/typeHelper"
 import { ResourceTypeSelectorProps } from "./interface"
-import { categoryStyle, containerStyle, resourceListStyle } from "./style"
+import {
+  categoryStyle,
+  containerStyle,
+  resourceListStyle,
+  whiteListButtonContainerStyle,
+  whiteListButtonStyle,
+  whiteListContentContainerStyle,
+  whiteListContentStyle,
+  whiteListDescriptionStyle,
+  whiteListOperationIconStyle,
+  whiteListTitleStyle,
+} from "./style"
 
 export const ResourceTypeSelector: FC<ResourceTypeSelectorProps> = (props) => {
   const { onSelect } = props
+  const { t } = useTranslation()
+  const message = createMessage()
+
+  const [showIPList, setShowIPList] = useState<boolean>(true)
+
+  const handleOperationIconClick = () => {
+    setShowIPList((prevState) => !prevState)
+  }
+  const handleCopyClick = useCallback(() => {
+    const copyResult = copy("")
+    if (copyResult) {
+      message.success({
+        content: t("copied"),
+      })
+    } else {
+      message.error({
+        content: t("copy_failed"),
+      })
+    }
+  }, [])
 
   return (
     <div css={containerStyle}>
@@ -25,6 +60,33 @@ export const ResourceTypeSelector: FC<ResourceTypeSelectorProps> = (props) => {
           </div>
         </div>
       ))}
+      <div>
+        <div css={whiteListContentContainerStyle}>
+          <div css={whiteListContentStyle}>
+            <div css={whiteListTitleStyle}>
+              {t("editor.action.resource.tip.allowlist.title")}
+            </div>
+            <div css={whiteListDescriptionStyle}>
+              {t("editor.action.resource.tip.allowlist.message")}
+            </div>
+          </div>
+          {isCloudVersion && (
+            <div css={whiteListButtonContainerStyle}>
+              <div css={whiteListButtonStyle} onClick={handleCopyClick}>
+                <CopyIcon />
+                <span>{t("editor.action.resource.button.copy_ip")}</span>
+              </div>
+              <span
+                css={whiteListOperationIconStyle}
+                onClick={handleOperationIconClick}
+              >
+                {showIPList ? <UpIcon /> : <DownIcon />}
+              </span>
+            </div>
+          )}
+        </div>
+        {isCloudVersion && showIPList && <div></div>}
+      </div>
     </div>
   )
 }
--- apps/builder/src/page/Dashboard/components/ResourceGenerator/ResourceTypeSelector/style.ts
+++ apps/builder/src/page/Dashboard/components/ResourceGenerator/ResourceTypeSelector/style.ts
@@ -1,5 +1,5 @@
 import { css } from "@emotion/react"
-import { getColor, globalColor, illaPrefix } from "@illa-design/react"
+import { getColor } from "@illa-design/react"
 
 export const containerStyle = css`
   width: 100%;
@@ -16,7 +16,7 @@ export const categoryStyle = css`
   padding-top: 8px;
   font-size: 14px;
   font-weight: 500;
-  color: ${globalColor(`--${illaPrefix}-grayBlue-04`)};
+  color: ${getColor("grayBlue", "04")};
 `
 
 export const resourceListStyle = css`
@@ -24,4 +24,67 @@ export const resourceListStyle = css`
   display: grid;
   grid-gap: 16px;
   grid-template-columns: repeat(auto-fill, minmax(184px, 1fr));
+`
+
+export const whiteListContentContainerStyle = css`
+  width: 100%;
+  padding-top: 16px;
+  display: flex;
+  height: 82px;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 8px;
+`
+
+export const whiteListContentStyle = css`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+`
+
+export const whiteListTitleStyle = css`
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 22px;
+  color: ${getColor("grayBlue", "02")};
+  height: 22px;
+`
+export const whiteListDescriptionStyle = css`
+  height: 40px;
+  font-size: 12px;
+  line-height: 20px;
+  color: ${getColor("grayBlue", "04")};
+`
+
+export const whiteListButtonContainerStyle = css`
+  width: 120px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  height: 32px;
+`
+export const whiteListOperationIconStyle = css`
+  width: 12px;
+  height: 12px;
+  cursor: pointer;
+  display: flex;
+`
+
+export const whiteListButtonStyle = css`
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 6px 16px;
+  gap: 8px;
+  width: 100px;
+  height: 32px;
+  cursor: pointer;
+  font-size: 12px;
+  background: ${getColor("grayBlue", "09")};
+  & > span {
+    line-height: 20px;
+    color: ${getColor("grayBlue", "02")};
+  }
 `
