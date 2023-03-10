4ca054ad555401f849b3aeedc70eeef4beac671f
shandamengcheng <zhumengcheng@illasoft.com>
2023-03-10 17:00:20 +0800

    feat: restApi modification

--- apps/builder/src/page/App/components/Actions/RestApiConfigElement/BasicAuthPanel/interface.ts
+++
@@ -1,7 +0,0 @@
-import { Control } from "react-hook-form"
-import { BasicAuth } from "@/redux/resource/restapiResource"
-
-export interface BasicAuthPanelProps {
-  auth?: BasicAuth
-  control: Control
-}
