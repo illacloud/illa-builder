6173b632377d6fbaee6516c9744896f2d3e1c903
AruSeito <weichen@illasoft.com>
2023-03-11 00:36:17 +0800

    fix: 🐛 can't show border and move bar when isDragging

--- apps/builder/src/page/App/components/ScaleSquare/utils/useMouseHover.ts
+++ apps/builder/src/page/App/components/ScaleSquare/utils/useMouseHover.ts
@@ -1,21 +1,25 @@
 import { MouseEvent, useCallback } from "react"
+import { useDragDropManager } from "react-dnd"
 import { useDispatch, useSelector } from "react-redux"
 import { getHoveredComponents } from "@/redux/config/configSelector"
 import { configActions } from "@/redux/config/configSlice"
 
 export const useMouseHover = () => {
   const hoveredComponents = useSelector(getHoveredComponents)
+  const dragDropManager = useDragDropManager()
+  const isDragging = dragDropManager.getMonitor().isDragging()
   const dispatch = useDispatch()
 
   const handleMouseEnter = useCallback(
     (e: MouseEvent<HTMLDivElement>) => {
+      if (isDragging) return
       const currentDisplayName =
         e.currentTarget.getAttribute("data-displayName")
       if (!currentDisplayName) return
       const newHoveredComponents = [...hoveredComponents, currentDisplayName]
       dispatch(configActions.updateHoveredComponent(newHoveredComponents))
     },
-    [dispatch, hoveredComponents],
+    [dispatch, hoveredComponents, isDragging],
   )
 
   const handleMouseLeave = useCallback(
