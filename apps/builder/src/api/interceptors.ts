d6bd607c2ec02cae4a1a62de1329627da68f97a6
xiaoyu <lavamint@outlook.com>
2023-03-22 17:44:28 +0800

    feat: add redirectUrl

--- apps/builder/src/api/interceptors.ts
+++ apps/builder/src/api/interceptors.ts
@@ -4,7 +4,7 @@ import {
   removeRequestPendingPool,
 } from "@/api/helpers/axiosPendingPool"
 import { ILLARoute } from "@/router"
-import { cloudUrl } from "@/router/routerConfig"
+import { cloudRedirect, cloudUrl } from "@/router/routerConfig"
 import { getAuthToken, removeAuthToken } from "@/utils/auth"
 import { isCloudVersion } from "@/utils/typeHelper"
 
@@ -29,6 +29,7 @@ export const fullFillInterceptor = (response: AxiosResponse) => {
 export const axiosErrorInterceptor = (error: AxiosError) => {
   const { response } = error
   if (!response) return Promise.reject(error)
+  const { pathname, href } = location
   const { status } = response
   switch (status) {
     // TODO: @aruseito maybe need custom error status, because of we'll have plugin to request other's api
@@ -36,9 +37,8 @@ export const axiosErrorInterceptor = (error: AxiosError) => {
       removeAuthToken()
       if (isCloudVersion) {
         // navigate to illa cloud
-        ILLARoute.navigate(cloudUrl)
+        ILLARoute.navigate(cloudRedirect)
       } else {
-        const { pathname } = location
         ILLARoute.navigate("/login", {
           replace: true,
           state: {
