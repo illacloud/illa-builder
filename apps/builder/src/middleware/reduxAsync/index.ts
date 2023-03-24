203973ddaeab34753fb871deef0d93a1b544c949
xiaoyu <lavamint@outlook.com>
2023-03-24 18:13:51 +0800

    feat: remove useless code

--- apps/builder/src/middleware/reduxAsync/index.ts
+++ apps/builder/src/middleware/reduxAsync/index.ts
@@ -1,6 +1,5 @@
 import { cloneDeep } from "lodash"
 import * as Redux from "redux"
-import { getGuideStatus } from "@/redux/guide/guideSelector"
 import { receiveMessage } from "./receiveMessages"
 import { sendMessage } from "./sendMessage"
 
@@ -9,19 +8,16 @@ export const reduxAsync: Redux.Middleware = (store) => (next) => (action) => {
   const typeList = type.split("/")
   const isRemoteAction = typeList[typeList.length - 1] === "remote"
   const currentAppID = store.getState().currentApp.appInfo.appId ?? ""
-  const isGuideOpen = getGuideStatus(store.getState())
   if (isRemoteAction) {
     receiveMessage(action, currentAppID)
     return next(action)
   }
   const prevRootState = cloneDeep(store.getState())
   const resp = next(action)
-  if (!isGuideOpen) {
-    try {
-      sendMessage(prevRootState, store.getState(), action)
-    } catch (e) {
-      console.log(e)
-    }
+  try {
+    sendMessage(prevRootState, store.getState(), action)
+  } catch (e) {
+    console.log(e)
   }
   return resp
 }
