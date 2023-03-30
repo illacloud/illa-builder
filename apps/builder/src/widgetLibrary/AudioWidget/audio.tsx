c342d5d9728ac4b83fe9ee2f5703cb10a2cf8dd9
xiaoyu <lavamint@outlook.com>
2023-03-30 16:13:42 +0800

    feat: move custom audio

--- apps/builder/src/widgetLibrary/AudioWidget/audio.tsx
+++ apps/builder/src/widgetLibrary/AudioWidget/audio.tsx
@@ -1,26 +1,9 @@
-import dayjs from "dayjs"
-import duration from "dayjs/plugin/Duration"
-import {
-  FC,
-  forwardRef,
-  useCallback,
-  useEffect,
-  useMemo,
-  useRef,
-  useState,
-} from "react"
+import { FC, forwardRef, useCallback, useEffect, useRef, useState } from "react"
 import { useTranslation } from "react-i18next"
 import ReactPlayer from "react-player"
-import { Loading, VideoPlayIcon } from "@illa-design/react"
-import {
-  audioStyle,
-  fullStyle,
-  loadingStyle,
-} from "@/widgetLibrary/AudioWidget/style"
+import { fullStyle, loadingStyle } from "@/widgetLibrary/AudioWidget/style"
 import { TooltipWrapper } from "@/widgetLibrary/PublicSector/TooltipWrapper"
 import { VideoWidgetProps, WrappedVideoProps } from "./interface"
-
-dayjs.extend(duration)
 
 export const WrappedAudio = forwardRef<ReactPlayer, WrappedVideoProps>(
   (props, ref) => {
@@ -41,24 +24,9 @@ export const WrappedAudio = forwardRef<ReactPlayer, WrappedVideoProps>(
     const { t } = useTranslation()
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
-    const [duration, setDuration] = useState(0)
-    const [currentTime, setCurrentTime] = useState(0)
 
     if (url === "") {
       return <div css={loadingStyle}>{t("widget.video.empty")}</div>
-    }
-
-    const durationStr = useMemo(() => {
-      if (!duration) return "00:00"
-      return dayjs.duration(duration, "seconds").format("mm:ss")
-    }, [duration])
-    const currentTimeStr = useMemo(() => {
-      if (!currentTime) return "00:00"
-      return dayjs.duration(duration, "seconds").format("mm:ss")
-    }, [currentTime])
-
-    const handleDuration = (duration: number) => {
-      const durationStr = dayjs.duration(duration, "seconds").format("mm:ss")
     }
 
     return (
@@ -71,13 +39,6 @@ export const WrappedAudio = forwardRef<ReactPlayer, WrappedVideoProps>(
         {/*//   <div css={loadingStyle}>{t("widget.video.fail")}</div>*/}
         {/*// )*/}
         {/*null}*/}
-        <div css={audioStyle}>
-          <VideoPlayIcon />
-          <span></span>
-          <span>
-            {currentTimeStr}/{durationStr}
-          </span>
-        </div>
         <ReactPlayer
           // style={loading || error ? { display: "none" } : undefined}
           // fallback={
@@ -102,20 +63,6 @@ export const WrappedAudio = forwardRef<ReactPlayer, WrappedVideoProps>(
           playing={autoPlay || playing}
           draggable={false}
           onReady={(player) => {
-            const duration = player.getDuration()
-            // transform seconds to time string, use Day.js
-            const durationStr = dayjs
-              .duration(duration, "seconds")
-              .format("mm:ss")
-
-            console.log(
-              durationStr,
-              player,
-              player.getSecondsLoaded(),
-              player.getCurrentTime(),
-              player.getInternalPlayer(),
-              "player",
-            )
             setLoading(false)
             setError(false)
             onReady()
@@ -126,15 +73,6 @@ export const WrappedAudio = forwardRef<ReactPlayer, WrappedVideoProps>(
           onError={() => {
             setLoading(false)
             setError(true)
-          }}
-          onDuration={setDuration}
-          onSeek={(seek) => {
-            console.log("seek", seek)
-          }}
-          onProgress={(progress) => {
-            console.log("progress", progress)
-            const { playedSeconds } = progress
-            setCurrentTime(playedSeconds)
           }}
         />
       </>
