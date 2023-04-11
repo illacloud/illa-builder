5e30a3dbe03c820b4ed8a6d4b97d5c3213bae63a
wangtaofeng <1507337624@qq.com>
2023-04-11 18:29:58 +0800

    fix: add map on cloud version

--- apps/builder/src/widgetLibrary/widgetBuilder.tsx
+++ apps/builder/src/widgetLibrary/widgetBuilder.tsx
@@ -1,3 +1,4 @@
+import { isCloudVersion } from "@/utils/typeHelper"
 import {
   AUDIO_EVENT_HANDLER_CONFIG,
   AUDIO_PANEL_CONFIG,
@@ -473,12 +474,15 @@ export const WidgetConfig: WidgetConfigs = {
     panelConfig: CAROUSEL_PANEL_CONFIG,
     eventHandlerConfig: CAROUSEL_EVENT_HANDLER_CONFIG,
   },
-  MAP_WIDGET: {
+}
+
+if (isCloudVersion) {
+  WidgetConfig["MAP_WIDGET"] = {
     widget: MapWidget,
     config: MAP_WIDGET_CONFIG,
     panelConfig: MAP_PANEL_CONFIG,
     eventHandlerConfig: MAP_EVENT_HANDLER_CONFIG,
-  },
+  }
 }
 
 export type WidgetType = keyof typeof WidgetConfig
