a9866947b51074fc72a648bc1c199fddfa82518e
wangtaofeng <1507337624@qq.com>
2023-04-24 19:39:12 +0800

    feat: add time pick & range time

---
+++ apps/builder/src/widgetLibrary/TimeRangeWidget/index.tsx
@@ -0,0 +1,4 @@
+export { TimeRangeWidget, WrappedTimeRange } from "./timeRange"
+export { TIME_RANGE_WIDGET_CONFIG } from "./widgetConfig"
+export { TIME_RANGE_PANEL_CONFIG } from "./panelConfig"
+export { TIME_RANGE_EVENT_HANDLER_CONFIG } from "./eventHandlerConfig"
