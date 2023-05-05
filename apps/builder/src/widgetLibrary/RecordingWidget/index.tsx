0c5d6d7fb581398390d962ee89e6e345032f8fc1
wangtaofeng <1507337624@qq.com>
2023-05-05 13:47:18 +0800

    feat: add record audio

---
+++ apps/builder/src/widgetLibrary/RecordingWidget/index.tsx
@@ -0,0 +1,4 @@
+export { RecordingWidget, WrappedRecording } from "./recording"
+export { RECORDING_PANEL_CONFIG } from "./panelConfig"
+export { RECORDING_WIDGET_CONFIG } from "./widgetConfig"
+export { RECORDING_HANDLER_CONFIG } from "./eventHandlerConfig"
