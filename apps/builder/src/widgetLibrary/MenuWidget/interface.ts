8ca1bdeb7884bd9864de4dd15e279508ca46ab18
smallSohoSolo <b3069741@gmail.com>
2023-05-05 20:57:17 +0800

    fix: build error

--- apps/builder/src/widgetLibrary/MenuWidget/interface.ts
+++ apps/builder/src/widgetLibrary/MenuWidget/interface.ts
@@ -1,10 +1,7 @@
-import { HTMLAttributes } from "react"
 import { MenuProps } from "@illa-design/react"
 import { BaseWidgetProps } from "@/widgetLibrary/interface"
 
-export interface WrappedMenuProps
-  extends HTMLAttributes<HTMLDivElement>,
-    MenuProps {}
+export interface WrappedMenuProps extends MenuProps {}
 
 export interface MenuWidgetProps
   extends Omit<WrappedMenuProps, "handleOnClickMenuItem" | "w" | "h">,
