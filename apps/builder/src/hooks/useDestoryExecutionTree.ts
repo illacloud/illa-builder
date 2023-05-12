5b6d330eec9506cd6528304608df73ccc6b62d6c
AruSeito <weichen@illasoft.com>
2023-05-12 15:14:43 +0800

    fix: 🐛 reset cursor position and dragshadow when leave room

--- apps/builder/src/hooks/useDestoryExecutionTree.ts
+++ apps/builder/src/hooks/useDestoryExecutionTree.ts
@@ -2,6 +2,8 @@ import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { configActions } from "@/redux/config/configSlice"
 import { actionActions } from "@/redux/currentApp/action/actionSlice"
+import { cursorActions } from "@/redux/currentApp/cursor/cursorSlice"
+import { dragShadowActions } from "@/redux/currentApp/dragShadow/dragShadowSlice"
 import { componentsActions } from "@/redux/currentApp/editor/components/componentsSlice"
 import { destroyExecutionTree } from "@/redux/currentApp/executionTree/executionListener"
 import { executionActions } from "@/redux/currentApp/executionTree/executionSlice"
@@ -15,6 +17,8 @@ export const useDestroyApp = () => {
       dispatch(configActions.resetConfig())
       dispatch(actionActions.resetActionReducer())
       dispatch(componentsActions.resetComponentsReducer())
+      dispatch(cursorActions.resetCursorReducer())
+      dispatch(dragShadowActions.resetDragShadowInfoReducer())
       destroyExecutionTree()
       removeAllActionPeriod()
     }
