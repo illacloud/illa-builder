2bbea68d8a07b3cbdf7c6b8227661e494c368cf5
wangtaofeng <1507337624@qq.com>
2023-05-25 17:36:40 +0800

    feat: add chat widget

---
+++ apps/builder/src/widgetLibrary/ChatWidget/index.ts
@@ -0,0 +1,3 @@
+export { CHAT_PANEL_CONFIG } from "./panelConfig"
+export { CHAT_WIDGET_CONFIG } from "./widgetConfig"
+export { CHAT_EVENT_HANDLER_CONFIG } from "./eventHandlerConfig"
---
+++ apps/builder/src/widgetLibrary/ChatWidget/options.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react"
+import { ReactComponent as DeleteSvg } from "@/assets/chat/delete.svg"
+import { ReactComponent as ReplaySvg } from "@/assets/chat/replay.svg"
+import { IMessageItem } from "./interface"
+import { optionsStyle } from "./style"
+
+export const Options: FC<IMessageItem> = ({
+  message,
+  toolbarDelete,
+  handleOnReplay,
+  toolbarReplay,
+  handleOnDelete,
+}) => {
+  return (
+    <div css={optionsStyle(Boolean(toolbarReplay && toolbarDelete))}>
+      {toolbarReplay && (
+        <div
+          onClick={() => {
+            handleOnReplay && handleOnReplay(message)
+          }}
+        >
+          <ReplaySvg />
+        </div>
+      )}
+      {toolbarDelete && (
+        <div
+          onClick={() => {
+            handleOnDelete && handleOnDelete(message)
+          }}
+        >
+          <DeleteSvg />
+        </div>
+      )}
+    </div>
+  )
+}
