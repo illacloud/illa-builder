32ee299cc78e8e3901fa5644a80ed96d4e0c11db
wangtaofeng <1507337624@qq.com>
2023-05-30 16:55:52 +0800

    fix: rich text update UI
    fix: update UI

--- apps/builder/src/widgetLibrary/RichTextWidget/widgetConfig.tsx
+++ apps/builder/src/widgetLibrary/RichTextWidget/widgetConfig.tsx
@@ -1,17 +1,17 @@
-import { ReactComponent as IconsWidgetIcon } from "@/assets/widgetCover/icon.svg"
+import { ReactComponent as RichTextWidgetIcon } from "@/assets/widgetCover/richText.svg"
 import i18n from "@/i18n/config"
 import { RESIZE_DIRECTION, WidgetConfig } from "@/widgetLibrary/interface"
 
 export const RICH_TEXT_WIDGET_CONFIG: WidgetConfig = {
   type: "RICH_TEXT_WIDGET",
   displayName: "richText",
-  widgetName: i18n.t("widget.richText.name"),
-  icon: <IconsWidgetIcon />,
-  resizeDirection: RESIZE_DIRECTION.VERTICAL,
+  widgetName: i18n.t("widget.rich_text_editor.name"),
+  icon: <RichTextWidgetIcon />,
+  resizeDirection: RESIZE_DIRECTION.ALL,
   keywords: ["Rich Text", "富文本"],
   sessionType: "INPUTS",
-  w: 30,
-  h: 50,
+  w: 35,
+  h: 65,
   defaults: {
     value: `<h1>Title</h1>\n<p>Content</p>`,
     hidden: false,
