63678a6b35113b39ad060a08b3b095df09f04949
wangtaofeng <1507337624@qq.com>
2023-07-04 18:32:36 +0800

    fix: textarea and other widget auto height

--- apps/builder/src/page/App/components/ScaleSquare/constant/widget.ts
+++ apps/builder/src/page/App/components/ScaleSquare/constant/widget.ts
@@ -4,3 +4,5 @@ export const CONTAINER_PADDING = 4
 export const DEFAULT_MIN_COLUMN = 1
 export const LIKE_CONTAINER_WIDGET_PADDING = 4
 export const LIST_ITEM_MARGIN_TOP = 8
+export const LABEL_TOP_UNIT_HEIGHT = 32
+export const VALIDATE_MESSAGE_HEIGHT = 33
