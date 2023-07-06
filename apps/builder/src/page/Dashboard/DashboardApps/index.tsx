cb97ce03e06f8ca2a65d34b788eae7e560afe0e9
xiaoyu-a <lavamint@outlook.com>
2023-07-06 10:44:33 +0800

    feat: update create app

--- apps/builder/src/page/Dashboard/DashboardApps/index.tsx
+++ apps/builder/src/page/Dashboard/DashboardApps/index.tsx
@@ -1,15 +1,17 @@
 import copy from "copy-to-clipboard"
 import { isBoolean } from "lodash"
-import { FC, Suspense, useEffect, useState } from "react"
+import { FC, Suspense, useCallback, useEffect, useState } from "react"
 import { useTranslation } from "react-i18next"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import {
   Await,
   useBeforeUnload,
   useLoaderData,
+  useNavigate,
   useParams,
 } from "react-router-dom"
 import { Button, useMessage } from "@illa-design/react"
+import { BASIC_APP_CONFIG } from "@/config/newAppConfig"
 import { Avatar } from "@/illa-public-component/Avatar"
 import {
   ILLA_MIXPANEL_BUILDER_PAGE_NAME,
@@ -21,11 +23,12 @@ import {
   ATTRIBUTE_GROUP,
   USER_ROLE,
 } from "@/illa-public-component/UserRoleUtils/interface"
-import { CreateNewModal } from "@/page/Dashboard/components/CreateNewModal"
 import { openGuideModal } from "@/page/Template/gideModeModal"
 import { getIsTutorialViewed } from "@/redux/currentUser/currentUserSelector"
+import { dashboardAppActions } from "@/redux/dashboard/apps/dashboardAppSlice"
 import { DashboardApp } from "@/redux/dashboard/apps/dashboardAppState"
 import { getCurrentTeamInfo } from "@/redux/team/teamSelector"
+import { fetchCreateApp } from "@/services/apps"
 import {
   track,
   trackPageDurationEnd,
@@ -47,12 +50,14 @@ export const DashboardApps: FC = () => {
   const { t } = useTranslation()
   const { teamIdentifier } = useParams()
   const message = useMessage()
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const { appList } = useLoaderData() as { appList: DashboardApp[] }
 
   const teamInfo = useSelector(getCurrentTeamInfo)
   const isTutorialViewed = useSelector(getIsTutorialViewed)
 
-  const [createNewModalVisible, setCreateNewModalVisible] = useState(false)
+  const [loading, setLoading] = useState(false)
 
   const currentUserRole = teamInfo?.myRole ?? USER_ROLE.VIEWER
 
@@ -67,6 +72,31 @@ export const DashboardApps: FC = () => {
     ATTRIBUTE_GROUP.APP,
     ACTION_MANAGE.CREATE_APP,
   )
+
+  const createApp = useCallback(() => {
+    if (loading) return
+    setLoading(true)
+    fetchCreateApp({
+      appName: "Untitled app",
+      initScheme: BASIC_APP_CONFIG,
+    })
+      .then(
+        (response) => {
+          dispatch(
+            dashboardAppActions.addDashboardAppReducer({
+              app: response.data,
+            }),
+          )
+          navigate(`/${teamIdentifier}/app/${response.data.appId}`)
+        },
+        () => {
+          message.error({ content: t("create_fail") })
+        },
+      )
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [loading, teamIdentifier, dispatch, navigate, message, t])
 
   if (
     isBoolean(isTutorialViewed) &&
@@ -99,64 +129,54 @@ export const DashboardApps: FC = () => {
   }, [canCreateApp])
 
   return (
-    <>
-      <div css={appsContainerStyle}>
-        <div css={listTitleContainerStyle}>
-          <div css={teamInfoContainerStyle}>
-            <Avatar
-              css={teamAvatarStyle}
-              avatarUrl={teamInfo?.icon}
-              name={teamInfo?.name}
-              id={teamInfo?.id}
-            />
-            <span css={listTitleStyle}>{teamInfo?.name}</span>
-          </div>
-          <div>
-            {isCloudVersion ? null : (
-              <Button
-                colorScheme="gray"
-                onClick={() => {
-                  copy(window.location.href)
-                  message.success({ content: t("link_copied") })
-                }}
-              >
-                {t("share")}
-              </Button>
-            )}
-            {canCreateApp ? (
-              <Button
-                ml="4px"
-                colorScheme="techPurple"
-                onClick={() => {
-                  setCreateNewModalVisible(true)
-                  track(
-                    ILLA_MIXPANEL_EVENT_TYPE.CLICK,
-                    ILLA_MIXPANEL_BUILDER_PAGE_NAME.APP,
-                    { element: "create_new_app" },
-                  )
-                }}
-              >
-                {t("create_new_app")}
-              </Button>
-            ) : null}
-          </div>
+    <div css={appsContainerStyle}>
+      <div css={listTitleContainerStyle}>
+        <div css={teamInfoContainerStyle}>
+          <Avatar
+            css={teamAvatarStyle}
+            avatarUrl={teamInfo?.icon}
+            name={teamInfo?.name}
+            id={teamInfo?.id}
+          />
+          <span css={listTitleStyle}>{teamInfo?.name}</span>
         </div>
-        <Suspense fallback={<DashBoardLoading />}>
-          <Await resolve={appList} errorElement={<DashboardErrorElement />}>
-            <AppsContentBody canEditApp={canEditApp} />
-          </Await>
-        </Suspense>
+        <div>
+          {isCloudVersion ? null : (
+            <Button
+              colorScheme="gray"
+              onClick={() => {
+                copy(window.location.href)
+                message.success({ content: t("link_copied") })
+              }}
+            >
+              {t("share")}
+            </Button>
+          )}
+          {canCreateApp ? (
+            <Button
+              ml="4px"
+              colorScheme="techPurple"
+              loading={loading}
+              onClick={() => {
+                track(
+                  ILLA_MIXPANEL_EVENT_TYPE.CLICK,
+                  ILLA_MIXPANEL_BUILDER_PAGE_NAME.APP,
+                  { element: "create_new_app" },
+                )
+                createApp()
+              }}
+            >
+              {t("create_new_app")}
+            </Button>
+          ) : null}
+        </div>
       </div>
-      <CreateNewModal
-        onCreateSuccess={() => {
-          setCreateNewModalVisible(false)
-        }}
-        visible={createNewModalVisible}
-        onVisibleChange={(visible) => {
-          setCreateNewModalVisible(visible)
-        }}
-      />
-    </>
+      <Suspense fallback={<DashBoardLoading />}>
+        <Await resolve={appList} errorElement={<DashboardErrorElement />}>
+          <AppsContentBody canEditApp={canEditApp} />
+        </Await>
+      </Suspense>
+    </div>
   )
 }
 
