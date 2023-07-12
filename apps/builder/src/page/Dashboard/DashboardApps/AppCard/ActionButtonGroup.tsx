db7edf90ec328cbe680472372a2c29d597adb23f
xiaoyu <lavamint@outlook.com>
2023-07-12 17:41:26 +0800

    feat: update AppCard

--- apps/builder/src/page/Dashboard/DashboardApps/AppCard/ActionButtonGroup.tsx
+++ apps/builder/src/page/Dashboard/DashboardApps/AppCard/ActionButtonGroup.tsx
@@ -1,4 +1,4 @@
-import { FC, MouseEvent } from "react"
+import { FC, MouseEvent, useMemo } from "react"
 import { useTranslation } from "react-i18next"
 import { useNavigate, useParams } from "react-router-dom"
 import { Button, Space } from "@illa-design/react"
@@ -20,23 +20,37 @@ export const ActionButtonGroup: FC<ActionButtonGroupProps> = (props) => {
   const { teamIdentifier } = useParams()
   const navigate = useNavigate()
 
-  const toDeployedApp = (e: MouseEvent) => {
-    e.stopPropagation()
-    track(ILLA_MIXPANEL_EVENT_TYPE.CLICK, ILLA_MIXPANEL_BUILDER_PAGE_NAME.APP, {
-      element: "app_launch",
-      parameter5: appInfo.appId,
-    })
-    navigate(`/${teamIdentifier}/deploy/app/${appInfo.appId}`)
-  }
+  const toDeployedApp = useMemo(
+    () => (e: MouseEvent) => {
+      e.stopPropagation()
+      track(
+        ILLA_MIXPANEL_EVENT_TYPE.CLICK,
+        ILLA_MIXPANEL_BUILDER_PAGE_NAME.APP,
+        {
+          element: "app_launch",
+          parameter5: appInfo.appId,
+        },
+      )
+      navigate(`/${teamIdentifier}/deploy/app/${appInfo.appId}`)
+    },
+    [navigate, teamIdentifier, appInfo.appId],
+  )
 
-  const toEditApp = (e: MouseEvent) => {
-    e.stopPropagation()
-    track(ILLA_MIXPANEL_EVENT_TYPE.CLICK, ILLA_MIXPANEL_BUILDER_PAGE_NAME.APP, {
-      element: "app_edit",
-      parameter5: appInfo.appId,
-    })
-    navigate(`/${teamIdentifier}/app/${appInfo.appId}`)
-  }
+  const toEditApp = useMemo(
+    () => (e: MouseEvent) => {
+      e.stopPropagation()
+      track(
+        ILLA_MIXPANEL_EVENT_TYPE.CLICK,
+        ILLA_MIXPANEL_BUILDER_PAGE_NAME.APP,
+        {
+          element: "app_edit",
+          parameter5: appInfo.appId,
+        },
+      )
+      navigate(`/${teamIdentifier}/app/${appInfo.appId}`)
+    },
+    [navigate, teamIdentifier, appInfo.appId],
+  )
 
   return (
     <Space
