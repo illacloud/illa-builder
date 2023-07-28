961f3a460482f9ea4891f2acf7f9e06b1ff47b49
xiaoyu <31204860+echoxyc@users.noreply.github.com>
2023-07-28 11:32:32 +0800

    feat: add template (#1978)

---
+++ apps/builder/src/config/template/TableGuide/index.ts
@@ -0,0 +1,4 @@
+import data from "@/config/template/TableGuide/data.json"
+import { CurrentAppResp } from "@/page/App/resp/currentAppResp"
+
+export const TABLE_GUIDE_DATA = data as unknown as CurrentAppResp
