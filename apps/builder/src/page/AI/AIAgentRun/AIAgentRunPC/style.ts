ddc354ca9d43cee081531d34575eaeb8351e8b51
smallSohoSolo <b3069741@gmail.com>
2023-08-11 14:40:34 +0800

    chore: change vh vw to 100%

--- apps/builder/src/page/AI/AIAgentRun/AIAgentRunPC/style.ts
+++ apps/builder/src/page/AI/AIAgentRun/AIAgentRunPC/style.ts
@@ -4,8 +4,8 @@ import { getColor } from "@illa-design/react"
 export const aiAgentContainerStyle = css`
   display: flex;
   flex-direction: row;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
 `
 
 export const leftPanelContainerStyle = css`
