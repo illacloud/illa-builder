0c3b12f68c6ed2d07e3c132026b2da81f5ff2d95
smallSohoSolo <b3069741@gmail.com>
2023-08-14 21:57:18 +0800

    feat: update markdown message

--- apps/builder/src/page/AI/components/MarkdownMessage/style.ts
+++ apps/builder/src/page/AI/components/MarkdownMessage/style.ts
@@ -3,12 +3,9 @@ import { getColor } from "@illa-design/react"
 
 export const markdownMessageStyle = css`
   max-width: 100%;
-  overflow-x: hidden;
-  word-break: break-word;
   color: ${getColor("grayBlue", "02")};
   font-size: 14px;
   font-style: normal;
-  white-space: pre-wrap;
   font-weight: 400;
   line-height: 22px;
 `
