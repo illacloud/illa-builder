dac4951769886eebb87ff6383fc76296bdc4e247
AruSeito <weichen@illasoft.com>
2023-08-31 19:29:56 +0800

    fix: 🐛 viewer can't show create button

--- apps/builder/src/page/Dashboard/components/TeamContentEmpty/index.tsx
+++ apps/builder/src/page/Dashboard/components/TeamContentEmpty/index.tsx
@@ -1,5 +1,8 @@
+import { USER_ROLE, getCurrentTeamInfo } from "@illa-public/user-data"
+import { isBiggerThanTargetRole } from "@illa-public/user-role-utils"
 import { FC } from "react"
 import { useTranslation } from "react-i18next"
+import { useSelector } from "react-redux"
 import {
   Button,
   Empty,
@@ -12,6 +15,7 @@ import { emptyStyle, emptyTextStyle } from "./style"
 
 export const TeamContentEmpty: FC<TeamContentEmptyProps> = (props) => {
   const { t } = useTranslation()
+  const currentTeamInfo = useSelector(getCurrentTeamInfo)!
 
   return (
     <Empty
@@ -20,17 +24,23 @@ export const TeamContentEmpty: FC<TeamContentEmptyProps> = (props) => {
       description={
         <div css={emptyStyle}>
           <div css={emptyTextStyle}>{t("new_dashboard.desc.blank")}</div>
-          <Button
-            colorScheme="grayBlue"
-            loading={props.loading}
-            variant="outline"
-            leftIcon={<PlusIcon size="10px" />}
-            onClick={() => {
-              props.navigate?.()
-            }}
-          >
-            {t("new_dashboard.button.blank")}
-          </Button>
+          {isBiggerThanTargetRole(
+            USER_ROLE.VIEWER,
+            currentTeamInfo.myRole,
+            false,
+          ) && (
+            <Button
+              colorScheme="grayBlue"
+              loading={props.loading}
+              variant="outline"
+              leftIcon={<PlusIcon size="10px" />}
+              onClick={() => {
+                props.navigate?.()
+              }}
+            >
+              {t("new_dashboard.button.blank")}
+            </Button>
+          )}
         </div>
       }
     />
