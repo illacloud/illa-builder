1b604f8e7a1aa08599b620312c355359cbe7214d
chenlongbo <chenlongbo@illasoft.com>
2023-10-07 17:49:34 +0800

    chore: update data grid

--- apps/builder/src/widgetLibrary/DataGridCommunityWidget/panelConfig.tsx
+++ apps/builder/src/widgetLibrary/DataGridCommunityWidget/panelConfig.tsx
@@ -1,4 +1,94 @@
+import {
+  HorizontalCenterIcon,
+  HorizontalEndIcon,
+  HorizontalStartIcon,
+} from "@illa-design/react"
+import i18n from "@/i18n/config"
 import { PanelConfig } from "@/page/App/components/InspectPanel/interface"
+import { VALIDATION_TYPES } from "@/utils/validationFactory"
 
 const baseWidgetName = "dataGridCommunity"
-export const DATA_GRID_COMMUNITY_PANEL_CONFIG: PanelConfig[] = []
+export const DATA_GRID_COMMUNITY_PANEL_CONFIG: PanelConfig[] = [
+  {
+    id: `${baseWidgetName}-data`,
+    groupName: i18n.t("editor.inspect.setter_group.data"),
+    children: [
+      {
+        id: `${baseWidgetName}-data-source`,
+        labelName: i18n.t("editor.inspect.setter_label.data_source"),
+        useCustomLayout: true,
+        attrName: "dataSource",
+        setterType: "DATA_SOURCE_SELECT_SETTER",
+      },
+      {
+        id: `${baseWidgetName}-basic-emptyState`,
+        labelName: i18n.t("editor.inspect.setter_label.empty_state"),
+        labelDesc: i18n.t("editor.inspect.setter_tooltip.placeholder"),
+        attrName: "emptyState",
+        setterType: "INPUT_SETTER",
+        expectedType: VALIDATION_TYPES.STRING,
+      },
+      {
+        id: `${baseWidgetName}-basic-loading`,
+        labelName: i18n.t("editor.inspect.setter_label.loading"),
+        labelDesc: i18n.t("editor.inspect.setter_tooltip.loading"),
+        attrName: "loading",
+        setterType: "DYNAMIC_SWITCH_SETTER",
+        expectedType: VALIDATION_TYPES.BOOLEAN,
+        openDynamic: true,
+        useCustomLayout: true,
+      },
+    ],
+  },
+  {
+    id: `${baseWidgetName}-column`,
+    groupName: i18n.t("editor.inspect.setter_group.column"),
+    children: [
+      {
+        id: `${baseWidgetName}-basic-columns`,
+        useCustomLayout: true,
+        attrName: "columns",
+        setterType: "COLUMN_SETTER",
+        openDynamic: true,
+        childrenSetter: [
+          {
+            id: `${baseWidgetName}-column-header`,
+            labelName: i18n.t("editor.inspect.setter_label.column_title"),
+            attrName: "header",
+            setterType: "INPUT_SETTER",
+            expectedType: VALIDATION_TYPES.STRING,
+          },
+          {
+            id: `${baseWidgetName}-column-enableSorting`,
+            labelName: i18n.t("editor.inspect.setter_label.enable_sorting"),
+            attrName: "enableSorting",
+            setterType: "DYNAMIC_SWITCH_SETTER",
+            expectedType: VALIDATION_TYPES.BOOLEAN,
+            openDynamic: true,
+            useCustomLayout: true,
+          },
+          {
+            id: `${baseWidgetName}-column-alignment`,
+            setterType: "RADIO_GROUP_SETTER",
+            labelName: i18n.t("editor.inspect.setter_label.label_alignment"),
+            attrName: "alignment",
+            options: [
+              {
+                label: <HorizontalStartIcon />,
+                value: "left",
+              },
+              {
+                label: <HorizontalCenterIcon />,
+                value: "center",
+              },
+              {
+                label: <HorizontalEndIcon />,
+                value: "right",
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+]
