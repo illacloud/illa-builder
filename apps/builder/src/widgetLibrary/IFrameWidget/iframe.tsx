ecb87c3a4a771256561bd58bba74f533c00866fd
chenlongbo <chenlongbo@illasoft.com>
2023-10-07 18:27:48 +0800

    fix: iframe pointer drag events

--- apps/builder/src/widgetLibrary/IFrameWidget/iframe.tsx
+++ apps/builder/src/widgetLibrary/IFrameWidget/iframe.tsx
@@ -1,11 +1,21 @@
 import { FC } from "react"
+import { useSelector } from "react-redux"
+import { getIsResizing } from "@/redux/currentApp/executionTree/executionSelector"
 import { IFrameWidgetProps } from "@/widgetLibrary/IFrameWidget/interface"
-import { iframeContainer } from "@/widgetLibrary/IFrameWidget/style"
+import { applyIframeContainer } from "@/widgetLibrary/IFrameWidget/style"
 
 export const IFrameWidget: FC<IFrameWidgetProps> = (props) => {
   const { src } = props
+
+  const isResizingGlobal = useSelector(getIsResizing)
+
   return (
-    <iframe allow="*" allowFullScreen={true} css={iframeContainer} src={src} />
+    <iframe
+      allow="*"
+      allowFullScreen={true}
+      css={applyIframeContainer(isResizingGlobal)}
+      src={src}
+    />
   )
 }
 IFrameWidget.displayName = "IFrameWidget"
