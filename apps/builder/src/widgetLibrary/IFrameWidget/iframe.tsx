588c672074940a61fd562fa226d4012d1f19079c
chenlongbo <chenlongbo@illasoft.com>
2023-10-07 18:15:18 +0800

    feat: add iframe allow field

--- apps/builder/src/widgetLibrary/IFrameWidget/iframe.tsx
+++ apps/builder/src/widgetLibrary/IFrameWidget/iframe.tsx
@@ -4,7 +4,9 @@ import { iframeContainer } from "@/widgetLibrary/IFrameWidget/style"
 
 export const IFrameWidget: FC<IFrameWidgetProps> = (props) => {
   const { src } = props
-  return <iframe css={iframeContainer} src={src} />
+  return (
+    <iframe allow="*" allowFullScreen={true} css={iframeContainer} src={src} />
+  )
 }
 IFrameWidget.displayName = "IFrameWidget"
 export default IFrameWidget
