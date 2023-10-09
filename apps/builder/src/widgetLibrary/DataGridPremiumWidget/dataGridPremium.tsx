c118192595fbb290c71d312313f33e848ebd86f3
chenlongbo <chenlongbo@illasoft.com>
2023-10-09 21:51:37 +0800

    chore: update data-grid

--- apps/builder/src/widgetLibrary/DataGridPremiumWidget/dataGridPremium.tsx
+++ apps/builder/src/widgetLibrary/DataGridPremiumWidget/dataGridPremium.tsx
@@ -1,7 +1,10 @@
-import { DataGridPremium } from "@mui/x-data-grid-premium"
+import { DataGridPremium, LicenseInfo } from "@mui/x-data-grid-premium"
+import { GridColDef } from "@mui/x-data-grid/models/colDef/gridColDef"
 import { isObject } from "lodash"
 import { FC, useMemo } from "react"
 import { BaseDataGridProps } from "./interface"
+
+LicenseInfo.setLicenseKey(import.meta.env.ILLA_MUI_LICENSE)
 
 export const DataGridPremiumWidget: FC<BaseDataGridProps> = (props) => {
   const { loading, dataSource, dataSourceJS, dataSourceMode } = props
@@ -41,18 +44,21 @@ export const DataGridPremiumWidget: FC<BaseDataGridProps> = (props) => {
     }
   }, [rawData])
 
-  const columns = useMemo(() => {
+  const columns: GridColDef<object>[] = useMemo(() => {
     if (arrayData.length == 0) {
       return []
     } else {
       return Object.keys(arrayData[0]).map((key) => {
         return {
           field: key,
+          minWidth: 100,
           headerName: key,
         }
       })
     }
   }, [arrayData])
+
+  console.log("longbo", arrayData, columns)
 
   return (
     <DataGridPremium rows={arrayData} columns={columns} loading={loading} />
