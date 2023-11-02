5a236c958b1d06d1a5020dcd1d57278b9eee64ee
AruSeito <weichen@illasoft.com>
2023-11-02 19:33:03 +0800

    fix: 🐛 event handler

--- apps/builder/src/widgetLibrary/PublicSector/TransformWidgetWrapper/index.tsx
+++ apps/builder/src/widgetLibrary/PublicSector/TransformWidgetWrapper/index.tsx
@@ -241,9 +241,17 @@ export const TransformWidgetWrapper: FC<TransformWidgetProps> = memo(
           "$dynamicAttrPaths",
           [],
         )
-        const needRunEvents = cloneDeep(originEvents).filter((originEvent) => {
-          return originEvent.eventType === eventType
-        })
+
+        const needRunEvents = cloneDeep(originEvents)
+          .filter((originEvent) => {
+            return originEvent.eventType === eventType
+          })
+          .map((originEvent) => {
+            return {
+              ...originEvent,
+              originEnable: originEvent.enabled,
+            }
+          })
         const finalContext =
           ILLAEditorRuntimePropsCollectorInstance.getCurrentPageCalcContext(
             otherCalcContext,
@@ -269,6 +277,7 @@ export const TransformWidgetWrapper: FC<TransformWidgetProps> = memo(
           path,
           otherCalcContext,
         )
+
         dynamicPaths?.forEach((path: string) => {
           const realPath = isFunction(formatPath)
             ? formatPath(path)
