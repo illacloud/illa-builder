55ed7e6509af714906187ebebc5e4c1177e71aed
Scenery <1507337624@qq.com>
2023-11-03 15:08:12 +0800

    feat: drive source support audio

--- apps/builder/src/page/App/components/InspectPanel/PanelSetters/DriveSourceGroupSetter/utils.tsx
+++ apps/builder/src/page/App/components/InspectPanel/PanelSetters/DriveSourceGroupSetter/utils.tsx
@@ -1,25 +1,31 @@
+import { ReactComponent as AudioIcon } from "@/assets/drive/audio.svg"
 import { ReactComponent as ImageIcon } from "@/assets/drive/image.svg"
 import { ReactComponent as PdfIcon } from "@/assets/drive/pdf.svg"
 import { ReactComponent as VideoIcon } from "@/assets/drive/video.svg"
+import i18n from "@/i18n/config"
 
 export const getUploadModeInfo = (widgetType: string) => {
   switch (widgetType) {
     case "IMAGE_WIDGET":
     case "CAROUSEL_WIDGET":
       return {
-        name: "choose a Image",
+        name: i18n.t("choose a image"),
         icon: <ImageIcon />,
       }
     case "PDF_WIDGET":
       return {
-        name: "choose a pdf",
+        name: i18n.t("choose a pdf"),
         icon: <PdfIcon />,
       }
     case "VIDEO_WIDGET":
+      return {
+        name: i18n.t("choose a video"),
+        icon: <VideoIcon />,
+      }
     case "AUDIO_WIDGET":
       return {
-        name: "choose a video",
-        icon: <VideoIcon />,
+        name: i18n.t("choose a audio"),
+        icon: <AudioIcon />,
       }
   }
 }
