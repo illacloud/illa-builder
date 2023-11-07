d7476f20babe75e0906dd94ad78cd3b7f67693bb
Scenery <1507337624@qq.com>
2023-11-07 19:45:10 +0800

    feat: remove pdf widget support drive

--- apps/builder/src/widgetLibrary/PdfWidget/utils.ts
+++
@@ -1,9 +0,0 @@
-import {
-  HTTP_REQUEST_PUBLIC_BASE_URL,
-  PUBLIC_DRIVE_REQUEST_PREFIX,
-} from "@illa-public/illa-net"
-
-export const isDriveURL = (url: string) => {
-  let prefix = HTTP_REQUEST_PUBLIC_BASE_URL + PUBLIC_DRIVE_REQUEST_PREFIX
-  return url.includes(prefix)
-}
