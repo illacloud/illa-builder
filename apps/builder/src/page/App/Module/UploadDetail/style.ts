e6c3cef6be01b5b18528f27b658e8c8812ea0abf
AruSeito <weichen@illasoft.com>
2023-11-08 16:06:10 +0800

    fix: 💄 update style

--- apps/builder/src/page/App/Module/UploadDetail/style.ts
+++ apps/builder/src/page/App/Module/UploadDetail/style.ts
@@ -3,17 +3,26 @@ import { getColor } from "@illa-design/react"
 
 export const uploadDetailContainerStyle = css`
   padding: 5px 16px;
+  padding-right: 10px;
   border-radius: 16px;
   background-color: ${getColor("white", "01")};
-  position: absolute;
-  bottom: 0;
-  right: 50%;
   color: ${getColor("grayBlue", "02")};
   display: flex;
   justify-content: space-between;
   align-items: center;
   border: 1px solid ${getColor("grayBlue", "08")};
   box-shadow: 0px 2px 16px 0px rgba(0, 0, 0, 0.16);
+  width: fit-content;
+  pointer-events: all;
+`
+
+export const uploadDetailOuterContainerStyle = css`
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  pointer-events: none;
 `
 
 export const controllerAreaContainerStyle = css`
