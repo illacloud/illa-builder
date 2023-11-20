232a9128b8b7b4424af87b9c7f766d2a0043ee72
Scenery <1507337624@qq.com>
2023-11-20 10:23:41 +0800

    feat: update list & add grid list widget

---
+++ apps/builder/src/widgetLibrary/GridListWidget/constants.ts
@@ -0,0 +1,242 @@
+export const DEFAULT_LIST = [
+  {
+    company: "🏠 UnitedHealth Group",
+    email: "📧 kaldiss6i@craigslist.org",
+    name: "Yank Vanin",
+    photo:
+      "https://images.pexels.com/photos/1181424/pexels-photo-1181424.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Outbound BDR",
+  },
+  {
+    company: "🏠 GitHub",
+    email: "📧 cthorold1i@ca.gov",
+    name: "Willey Swatridge",
+    photo:
+      "https://images.pexels.com/photos/2853592/pexels-photo-2853592.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Data Analyst",
+  },
+  {
+    company: "🏠 Chevron",
+    email: "📧 nvlasyuk5d@berkeley.edu",
+    name: "Piper Whisker",
+    photo:
+      "https://images.pexels.com/photos/1181690/pexels-photo-1181690.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Business Analyst",
+  },
+  {
+    company: "🏠 Gong",
+    email: "📧 dscanterburyx@topsy.com",
+    name: "Sargent Boswell",
+    photo:
+      "https://images.pexels.com/photos/1586973/pexels-photo-1586973.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Internal Tools Lead",
+  },
+  {
+    company: "🏠 Boeing",
+    email: "📧 ederobertis2j@blogspot.com",
+    name: "Anita Mordy",
+    photo:
+      "https://images.pexels.com/photos/1181391/pexels-photo-1181391.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Founder",
+  },
+  {
+    company: "🏠 Valero Energy",
+    email: "📧 cfoale16@oracle.com",
+    name: "Domini Queree",
+    photo:
+      "https://images.pexels.com/photos/826349/pexels-photo-826349.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Account Executive",
+  },
+  {
+    company: "🏠 Citigroup",
+    email: "📧 hmacquire3y@fema.gov",
+    name: "Hunter Dossett",
+    photo:
+      "https://images.pexels.com/photos/3769021/pexels-photo-3769021.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Outbound BDR",
+  },
+  {
+    company: "🏠 Twitter",
+    email: "📧 eupward4x@php.net",
+    name: "Phylis Bonass",
+    photo:
+      "https://images.pexels.com/photos/806835/pexels-photo-806835.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Infrastructure Engineer",
+  },
+  {
+    company: "🏠 Intel",
+    email: "📧 nbexley6g@mayoclinic.com",
+    name: "Lyndsey Bransom",
+    photo:
+      "https://images.pexels.com/photos/920382/pexels-photo-920382.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Co-Founder",
+  },
+  {
+    company: "🏠 Target",
+    email: "📧 dscanterburyx@topsy.com",
+    name: "Pepi Peter",
+    photo:
+      "https://images.pexels.com/photos/1586996/pexels-photo-1586996.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Database Manager",
+  },
+  {
+    company: "🏠 Oracle",
+    email: "📧 vhefforde46@fc2.com",
+    name: "Way Guy",
+    photo:
+      "https://images.pexels.com/photos/785667/pexels-photo-785667.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Backend Engineer",
+  },
+  {
+    company: "🏠 Uber",
+    email: "📧 uqueree69@answers.com",
+    name: "Karalee Coton",
+    photo:
+      "https://images.pexels.com/photos/914931/pexels-photo-914931.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Project Manager",
+  },
+  {
+    company: "🏠 Walmart",
+    email: "📧 pravillas4l@yale.edu",
+    name: "Daniele Adamik",
+    photo:
+      "https://images.pexels.com/photos/1024403/pexels-photo-1024403.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Customer Success Team Lead",
+  },
+  {
+    company: "🏠 Dell Technologies",
+    email: "📧 nolfert4o@pbs.org",
+    name: "Auria Archer",
+    photo:
+      "https://images.pexels.com/photos/1181414/pexels-photo-1181414.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 CMO",
+  },
+  {
+    company: "🏠 PepsiCo",
+    email: "📧 fradleigh22@shop-pro.jp",
+    name: "Caesar Jakubowsky",
+    photo:
+      "https://images.pexels.com/photos/834863/pexels-photo-834863.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Sales Engineer",
+  },
+  {
+    company: "🏠 Bank of America",
+    email: "📧 prawlingson5m@imgur.com",
+    name: "Noelle Loache",
+    photo:
+      "https://images.pexels.com/photos/3756679/pexels-photo-3756679.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 CMO",
+  },
+  {
+    company: "🏠 Morgan Stanley",
+    email: "📧 sbeyn1d@liveinternet.ru",
+    name: "Benedetta Warbrick",
+    photo:
+      "https://images.pexels.com/photos/32976/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Business Analyst",
+  },
+  {
+    company: "🏠 Nationwide",
+    email: "📧 afissenden1j@weibo.com",
+    name: "Lyndsey Pycock",
+    photo:
+      "https://images.pexels.com/photos/3184608/pexels-photo-3184608.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Backend Engineer",
+  },
+  {
+    company: "🏠 Front",
+    email: "📧 rpeetz@npr.org",
+    name: "Stuart Eymer",
+    photo:
+      "https://images.pexels.com/photos/1181695/pexels-photo-1181695.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Database Manager",
+  },
+  {
+    company: "🏠 Amazon",
+    email: "📧 clittrell5r@bigcartel.com",
+    name: "Mar Rogeon",
+    photo:
+      "https://images.pexels.com/photos/50855/pexels-photo-50855.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Inbound BDR",
+  },
+  {
+    company: "🏠 HP",
+    email: "📧 lstovine51@irs.gov",
+    name: "Tom Warbrick",
+    photo:
+      "https://images.pexels.com/photos/789822/pexels-photo-789822.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Solutions Architect",
+  },
+  {
+    company: "🏠 State Farm Insurance",
+    email: "📧 danten3q@bbc.co.uk",
+    name: "Fabien Aitken",
+    photo:
+      "https://images.pexels.com/photos/1181678/pexels-photo-1181678.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Business Analyst",
+  },
+  {
+    company: "🏠 Retool",
+    email: "📧 gfraschetti54@independent.co.uk",
+    name: "Kipp Fendt",
+    photo:
+      "https://images.pexels.com/photos/1181293/pexels-photo-1181293.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Customer Engineer",
+  },
+  {
+    company: "🏠 Stripe",
+    email: "📧 ahucklesby1v@ucsd.edu",
+    name: "Guillermo Anten",
+    photo:
+      "https://images.pexels.com/photos/7647990/pexels-photo-7647990.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Customer Success Team Lead",
+  },
+  {
+    company: "🏠 Nike",
+    email: "📧 kaitkeny@telegraph.co.uk",
+    name: "Micah McLeod",
+    photo:
+      "https://images.pexels.com/photos/2955376/pexels-photo-2955376.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Sales Engineer",
+  },
+  {
+    company: "🏠 Johnson & Johnson",
+    email: "📧 chelgass33@hhs.gov",
+    name: "Ivar Filon",
+    photo:
+      "https://images.pexels.com/photos/7648004/pexels-photo-7648004.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Support Specialist",
+  },
+  {
+    company: "🏠 Cisco Systems",
+    email: "📧 tmenzies6p@hexun.com",
+    name: "Micah Bohey",
+    photo:
+      "https://images.pexels.com/photos/7647992/pexels-photo-7647992.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Support Specialist",
+  },
+  {
+    company: "🏠 General Motors",
+    email: "📧 chelgass33@hhs.gov",
+    name: "Georgianne Guy",
+    photo:
+      "https://images.pexels.com/photos/8145344/pexels-photo-8145344.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Internal Tools Lead",
+  },
+  {
+    company: "🏠 Wells Fargo",
+    email: "📧 ddurrett42@yahoo.com",
+    name: "Tom Raffels",
+    photo:
+      "https://images.pexels.com/photos/8134106/pexels-photo-8134106.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Inbound BDR",
+  },
+  {
+    company: "🏠 Costco Wholesale",
+    email: "📧 svanine@flavors.me",
+    name: "Annadiana Brockman",
+    photo:
+      "https://images.pexels.com/photos/7644155/pexels-photo-7644155.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    title: "🧑‍💻 Support Specialist",
+  },
+]
--- apps/builder/src/widgetLibrary/GridListWidget/widgetConfig.tsx
+++ apps/builder/src/widgetLibrary/GridListWidget/widgetConfig.tsx
@@ -1,6 +1,5 @@
-import { ReactComponent as ListWidgetIcon } from "@/assets/widgetCover/list.svg"
+import { ReactComponent as GridListWidgetIcon } from "@/assets/widgetCover/gridList.svg"
 import i18n from "@/i18n/config"
-import { LIST_ITEM_MARGIN_TOP } from "@/page/App/components/ScaleSquare/constant/widget"
 import { TEMPLATE_DISPLAYNAME_KEY } from "@/utils/generators/generateComponentNode"
 import { BasicContainerConfig } from "@/widgetLibrary/BasicContainer/BasicContainer"
 import { IMAGE_WIDGET_CONFIG } from "@/widgetLibrary/ImageWidget"
@@ -9,16 +8,16 @@ import { RESIZE_DIRECTION, WidgetConfig } from "@/widgetLibrary/interface"
 import { DEFAULT_LIST } from "./constants"
 import { PAGINATION_TYPE } from "./interface"
 
-export const LIST_WIDGET_CONFIG: WidgetConfig = {
-  type: "LIST_WIDGET",
-  displayName: "list",
-  widgetName: i18n.t("widget.list.name"),
-  keywords: ["list", "列表"],
-  icon: <ListWidgetIcon />,
+export const GRID_LIST_WIDGET_CONFIG: WidgetConfig = {
+  type: "GRID_LIST_WIDGET",
+  displayName: "gridList",
+  widgetName: i18n.t("widget.grid_list.name"),
+  keywords: ["gridList", "网格列表"],
+  icon: <GridListWidgetIcon />,
   sessionType: "CONTAINER",
   w: 13,
   h: 38,
-  version: 1,
+  version: 0,
   childrenNode: [
     {
       ...BasicContainerConfig,
@@ -91,7 +90,6 @@ export const LIST_WIDGET_CONFIG: WidgetConfig = {
     enablePagination: "{{true}}",
     enableServerSidePagination: false,
     pageSize: "{{10}}",
-    itemGap: LIST_ITEM_MARGIN_TOP,
     paginationType: PAGINATION_TYPE.LIMIT_OFFSET_BASED,
     itemBackGroundColor: "white",
     backgroundColor: "white",
@@ -99,8 +97,8 @@ export const LIST_WIDGET_CONFIG: WidgetConfig = {
     selectedIndex: undefined,
     selectedItem: undefined,
     dataSources: `{{
-  ${JSON.stringify(DEFAULT_LIST)}
-}}`,
+      ${JSON.stringify(DEFAULT_LIST)}
+    }}`,
     dynamicHeight: "fixed",
     resizeDirection: RESIZE_DIRECTION.ALL,
     page: "{{0}}",
