d601ce7974f733a13d7b3a47e7c9522898739950
AruSeito <41471697+AruSeito@users.noreply.github.com>
2023-12-15 11:46:33 +0800

    feat: ✨ update axios

--- apps/builder/src/api/http/base.ts
+++ apps/builder/src/api/http/base.ts
@@ -1,6 +1,9 @@
-import { actionRuntimeAxios, needAuthAxios } from "@illa-public/illa-net/base"
-import { authInterceptor } from "@/api/http/interceptors/request/auth"
-import { errorHandlerInterceptor } from "@/api/http/interceptors/response/errorHandler"
+import {
+  actionRuntimeAxios,
+  authInterceptor,
+  errorHandlerInterceptor,
+  needAuthAxios,
+} from "@illa-public/illa-net"
 
 needAuthAxios.interceptors.request.use(authInterceptor)
 needAuthAxios.interceptors.response.use(undefined, errorHandlerInterceptor)
--- apps/cloud/src/api/http/base.ts
+++ apps/cloud/src/api/http/base.ts
@@ -1,6 +1,8 @@
-import { needAuthAxios } from "@illa-public/illa-net"
-import { authInterceptor } from "./interceptors/request/auth"
-import { errorHandlerInterceptor } from "./interceptors/response/errorHandler"
+import {
+  authInterceptor,
+  errorHandlerInterceptor,
+  needAuthAxios,
+} from "@illa-public/illa-net"
 
 needAuthAxios.interceptors.request.use(authInterceptor)
 needAuthAxios.interceptors.response.use(undefined, errorHandlerInterceptor)
