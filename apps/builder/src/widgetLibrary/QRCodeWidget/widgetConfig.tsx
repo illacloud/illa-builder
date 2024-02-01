0e1a724e29fdabb5d623054cf336ed6560293696
Scenery <1507337624@qq.com>
2024-02-01 20:25:22 +0800

    feat: update translation key

--- apps/builder/src/widgetLibrary/QRCodeWidget/widgetConfig.tsx
+++ apps/builder/src/widgetLibrary/QRCodeWidget/widgetConfig.tsx
@@ -1,15 +1,15 @@
-import RadioButtonWidgetIcon from "@/assets/widgetCover/radioButton.svg?react"
+import QrCodeWidgetIcon from "@/assets/widgetCover/qrcode.svg?react"
 import i18n from "@/i18n/config"
 import { RESIZE_DIRECTION, WidgetConfig } from "@/widgetLibrary/interface"
 
 export const QR_CODE_WIDGET_CONFIG: WidgetConfig = {
   type: "QR_CODE_WIDGET",
-  widgetName: i18n.t("qrCode"),
+  widgetName: i18n.t("widget.qr_code.name"),
   displayName: "qrCode",
   w: 8,
-  h: 26,
+  h: 32,
   resizeDirection: RESIZE_DIRECTION.HORIZONTAL,
-  icon: <RadioButtonWidgetIcon />,
+  icon: <QrCodeWidgetIcon />,
   keywords: ["QR code", "二维码生成器"],
   sessionType: "PRESENTATION",
   version: 0,
--- apps/builder/src/widgetLibrary/widgetBuilder.tsx
+++ apps/builder/src/widgetLibrary/widgetBuilder.tsx
@@ -536,6 +536,12 @@ export const WidgetConfigMap: Record<
     eventHandlerConfig: BUTTON_EVENT_HANDLER_CONFIG,
     widget: lazy(() => import("@/widgetLibrary/ButtonWidget/button")),
   },
+  QR_CODE_WIDGET: {
+    config: QR_CODE_WIDGET_CONFIG,
+    panelConfig: QR_CODE_PANEL_CONFIG,
+    eventHandlerConfig: QR_CODE_EVENT_HANDLER_CONFIG,
+    widget: lazy(() => import("@/widgetLibrary/QRCodeWidget/qrCode")),
+  },
   TAGS_WIDGET: {
     config: TAGS_WIDGET_CONFIG,
     panelConfig: TAGS_PANEL_CONFIG,
@@ -639,12 +645,6 @@ export const WidgetConfigMap: Record<
     panelConfig: IFRAME_PANEL_CONFIG,
     eventHandlerConfig: IFRAME_EVENT_HANDLER_CONFIG,
     widget: lazy(() => import("@/widgetLibrary/IFrameWidget/iframe")),
-  },
-  QR_CODE_WIDGET: {
-    config: QR_CODE_WIDGET_CONFIG,
-    panelConfig: QR_CODE_PANEL_CONFIG,
-    eventHandlerConfig: QR_CODE_EVENT_HANDLER_CONFIG,
-    widget: lazy(() => import("@/widgetLibrary/QRCodeWidget/qrCode")),
   },
 }
 
