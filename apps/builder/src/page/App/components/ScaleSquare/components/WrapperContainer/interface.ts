952b6dae7ffce8bef6362f8725591b195a9aaaa3
AruSeito <41471697+AruSeito@users.noreply.github.com>
2024-02-21 14:18:03 +0800

    fix: 🐛 multi select

--- apps/builder/src/page/App/components/ScaleSquare/components/WrapperContainer/interface.ts
+++ apps/builder/src/page/App/components/ScaleSquare/components/WrapperContainer/interface.ts
@@ -2,7 +2,6 @@ import { ReactNode } from "react"
 
 export interface WrapperContainerProps {
   displayName: string
-  parentNodeDisplayName: string
   widgetHeight: number
   children: ReactNode
 }
