10a3947dfb2d0cb1604b3456bb1673d131430871
Scenery <1507337624@qq.com>
2024-02-23 19:48:37 +0800

    feat: add signature

---
+++ apps/builder/src/widgetLibrary/SignatureWidget/interface.tsx
@@ -0,0 +1,27 @@
+import { PADDING_MODE } from "@illa-public/public-types"
+import { ValidateMessageOldProps } from "@/widgetLibrary/PublicSector/InvalidMessage/interface"
+import LabelProps from "@/widgetLibrary/PublicSector/Label/interface"
+import { TooltipWrapperProps } from "@/widgetLibrary/PublicSector/TooltipWrapper/interface"
+import { BaseWidgetProps } from "@/widgetLibrary/interface"
+
+export interface WrappedSignatureProps {
+  penColor?: string
+  guideColor?: string
+  text?: string
+  disabled?: boolean
+  value?: string
+  handleUpdateSignature: (value?: string, dataURI?: string) => void
+}
+
+export interface SignatureWidgetProps
+  extends WrappedSignatureProps,
+    BaseWidgetProps,
+    LabelProps,
+    TooltipWrapperProps,
+    Omit<ValidateMessageOldProps, "value"> {
+  validateMessage: string
+  padding?: {
+    size: string
+    mode: PADDING_MODE
+  }
+}
